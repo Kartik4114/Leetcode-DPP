de0133081696a4b517a536ba22760d3baf89e393
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-05-21 22:54:52 +0530

    Time: 3 ms (90.34%), Space: 11.9 MB (56.85%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+
+        // Approach 1:- using unordered map
+        // Approach 2:- using sorting
+        
+        // Approach 3 :- BIT MANIPULATION (BEST APPROACH)
+//        T.C:- O(n) , S.C:- O(1)
+        
         int result=0;
 
         for(int k=0;k<=31;k++){
