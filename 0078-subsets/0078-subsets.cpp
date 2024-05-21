14167357c305303919e5edb192563e69ea045434
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-05-21 17:11:33 +0530

    Time: 2 ms (53.70%), Space: 8.3 MB (62.90%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -12,14 +12,35 @@ public:
         temp.pop_back();
 
         a1(nums,i+1,temp);
-
         
+    }
+
+    vector<vector<int>> a2(vector<int> &nums){
+        // counting size of subsets
+        int sub_size=pow(2,nums.size());
+        vector<vector<int>> result;
+
+        for(int i=0;i<sub_size;i++){
+            vector<int> temp;
+
+            for(int j=0;j<nums.size();j++){
+                if(i & (1<<j)) temp.push_back(nums[j]);
+            }
+            result.push_back(temp);
+        }
+
+        return result;
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         // APPROACH 1:- USING RECURSION 
-            vector<int> temp;
-            a1(nums,0,temp);
+        // T.C :- O(2^n) , S.C:- O(2^n)
+            // vector<int> temp;
+            // a1(nums,0,temp);
 
-            return ans;
+            // return ans;
+        
+        // APPROACH 2:- USING BIT MANIPULATION
+        // T.C :- O(n*(2^n)) , S.C:- O(n*(2^n))
+            return a2(nums);
     }
 };
