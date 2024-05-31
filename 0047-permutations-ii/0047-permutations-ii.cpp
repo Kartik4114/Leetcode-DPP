7f106922b9d8bb9048ce7b92c670024568e72d5a
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-05-31 16:18:56 +0530

    Time: 7 ms (57.84%), Space: 15 MB (15.33%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -6,7 +6,7 @@ public:
     void a1(vector<int> &nums,vector<int> &temp){
         int n=nums.size();
         if(temp.size()==n){
-            result.push_back(temp);
+            result.push_back(temp); // O(n)
             return;
         }
 
@@ -22,17 +22,41 @@ public:
             count++;
         }
     }
+
+    void a2(vector<int> &nums,int idx){
+        int n=nums.size();
+        if(idx==n){
+            result.push_back(nums);
+            return;
+        }
+
+        unordered_set<int> st;
+        for(int i=idx;i<n;i++){
+            if(st.find(nums[i])!=st.end())  continue;
+
+            st.insert(nums[i]);
+            swap(nums[i],nums[idx]);
+
+            a2(nums,idx+1);
+
+            swap(nums[i],nums[idx]);
+
+        }
+    }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         
         // APPROACH 1:- USING UNORDERED MAP 
+        // T.C :- O(n*n!) , S.C:- O(n) Auxillary space
 
             vector<int> temp;
             for(auto num:nums){
                 mp[num]++;
             }
-            a1(nums,temp);
+            // a1(nums,temp);
+        
+        // APPROACH 2 :- USING SWAPPING METHOD IN RECURSION
 
+            a2(nums,0);
             return result;
-
     }
 };
