c70173dd2869c37e3ead42afa061275cd5f8d4e1
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-06-03 15:25:46 +0530

    Time: 4 ms (31.74%), Space: 9.1 MB (81.4%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -6,17 +6,43 @@ public:
 
         if(memo[i]!=-1) return memo[i];
         
-        int ans1=a1(nums,i+2)+nums[i];
-        int ans2=a1(nums,i+1);
+        int steal=a1(nums,i+2)+nums[i];
+        int skip=a1(nums,i+1);
 
-        return memo[i]=max(ans1,ans2);
+        return memo[i]=max(steal,skip);
+
+    }
+
+    int a2(vector<int> &nums){
+        
+        
+        int n=nums.size();
+
+        if(n==1) return nums[0];
+        vector<int> dp(n+1,0);
+        dp[0]=0; // it means 0 houses
+        dp[1]=nums[0]; // it means 1 house is present
+
+        for(int i=2;i<n+1;i++){
+            
+            int steal=dp[i-2]+nums[i-1];
+            int skip=dp[i-1];
+
+            dp[i]=max(steal,skip);
+
+        }
+
+        return dp[n];
 
     }
     int rob(vector<int>& nums) {
         
         // APPROACH 1:- USING RECURSION +MEMOIZATION
         memset(memo,-1,sizeof(memo));
-        return a1(nums,0);
+        // return a1(nums,0);
+
+        // APPROACH 2:- USING DYNAMIC PROGRAMMING 
+        return a2(nums);
         
     }
 };
