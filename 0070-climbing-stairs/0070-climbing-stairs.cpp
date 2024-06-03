4e8d7f0391f113a085beb03aea6b3ea71efaea0c
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-06-03 13:47:00 +0530

    Time: 3 ms (29.34%), Space: 7.5 MB (11.43%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -13,10 +13,29 @@ public:
 
         return memo[n]=ans;
     }
+
+    int a2(int n){
+        
+        if(n<=2) return n;
+        vector<int> dp(n+1);
+        
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+
+    }
     int climbStairs(int n) {
         
         // APPROACH 1 :- USING RECURSION + MEMOIZATION
-        memset(memo,-1,sizeof(memo));
-        return a1(n);
+        // memset(memo,-1,sizeof(memo));
+        // return a1(n);
+
+        // APPROACH 2 :- USING DYNAMIC PROGRAMMING
+        return a2(n);
     }
 };
