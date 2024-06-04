772c861ae51520d61020847c982598a80ef6c83f
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-06-04 21:53:12 +0530

    Time: 284 ms (15.33%), Space: 157.2 MB (28.89%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
@@ -21,6 +21,23 @@ public:
         ll take=a1(nums,idx+1,!flag)+val;
         return memo[idx][flag]=max(skip,take);
     }
+
+    ll a2(vector<int> &nums){
+        int n=nums.size();
+        vector<vector<ll>> dp(n+1,vector<ll> (2,0));
+
+        for(int i=1;i<=n;i++){
+
+            // Even (0 is for even)
+            dp[i][0]= max(dp[i-1][1]-nums[i-1],dp[i-1][0]);
+
+            // Odd (0 is for Odd)
+            dp[i][1]= max(dp[i-1][0]+nums[i-1],dp[i-1][1]);
+        }
+
+        return max(dp[n][0],dp[n][1]);
+
+    }
     long long maxAlternatingSum(vector<int>& nums) {
         
         // APPROACH 1: -USING RECURSION +MEMOIZATION
@@ -29,6 +46,11 @@ public:
         // IN MEMOIZATION WE WILL TAKE 2-D ARRAY 1 FOR IDX AND 2ND FOR FLAG(TRUE OR FALSE)
 
         memset(memo,-1,sizeof(memo));
-        return a1(nums,0,true);
+        // return a1(nums,0,true);
+
+        // APPROACH 2: -USING DYNAMIC PROGRAMMING
+            return a2(nums);
+
+
     }
 };
