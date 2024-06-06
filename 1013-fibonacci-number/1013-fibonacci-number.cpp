d8ed768105cf82015c08cf32da6830ebe508ee81
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-06-06 12:15:06 +0530

    Time: 0 ms (100%), Space: 7.2 MB (34.27%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-
-    int fib_helper(int n,int*arr){
-        
-        if(n==0){
-            return 0;  
-        }
-        if(n==1){
-            return 1;
-        }
-        if(arr[n]!=-1){
-            return arr[n];
-        }
-
-        int a=fib_helper(n-1,arr);
-        int b=fib_helper(n-2,arr);
-        arr[n]=a+b;
-
-        return arr[n];
-    }
     int fib(int n) {
-        int *arr=new int[n+1];
-        for(int i=0;i<=n;i++){
-            arr[i]=-1;
+        
+        if(n<=1) return n;
+
+        int a=0;
+        int b=1;
+
+        int c;
+        for(int i=1;i<n;i++){
+            c=a+b;
+
+            a=b;
+            b=c;
+
         }
-        return fib_helper(n,arr);
+
+        return c;
+        
     }
 };
