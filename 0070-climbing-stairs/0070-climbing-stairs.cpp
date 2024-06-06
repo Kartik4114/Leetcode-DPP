d356d526d127524bba0bbc59377b5922c235816e
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-06-06 12:18:04 +0530

    Time: 3 ms (29.52%), Space: 7.1 MB (86.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,59 +1,22 @@
 class Solution {
 public:
     int memo[46];
-    int a1(int n){
-        
+    int solve(int n){
+
         if(n<=2) return n;
 
         if(memo[n]!=-1) return memo[n];
 
-        int ans=0;
-        ans+=a1(n-1);
-        ans+=a1(n-2);
+        int a=solve(n-1);
+        int b=solve(n-2);
 
-        return memo[n]=ans;
-    }
-
-    int a2(int n){
-        
-        if(n<=2) return n;
-        vector<int> dp(n+1);
-        
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-
-        for(int i=3;i<n+1;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        return memo[n]=a+b;
 
     }
-
-    int a3(int n){
-        if(n<=2) return n;
-
-        int a=1,b=2,c=3;
-
-        for(int i=3;i<n+1;i++){
-            c=a+b;
-            int temp_b=b;
-            b=c;
-            a=temp_b;
-        }
-        return c;
-
-    }
-    int climbStairs(int n) {
+    int climbStairs(int n) {   
         
-        // APPROACH 1 :- USING RECURSION + MEMOIZATION
         memset(memo,-1,sizeof(memo));
-        // return a1(n);
+        return solve(n);
 
-        // APPROACH 2 :- USING DYNAMIC PROGRAMMING
-        // return a2(n);
-
-        // APPROACH 3:- WITHOUT USING dp ARRAY
-        return a3(n);
     }
 };
