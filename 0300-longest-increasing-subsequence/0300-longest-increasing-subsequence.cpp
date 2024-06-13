05f89329bbdf540c08406edcb31c5fd3e6a776b3
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-06-13 14:03:23 +0530

    Time: 421 ms (23.77%), Space: 40.4 MB (32.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,60 +1,31 @@
 class Solution {
 public:
     int memo[2501][2501];
-    
-    int a1(vector<int> &nums, int idx, int prev_idx) {
-        int n = nums.size();
-        if (idx == n) return 0;
-
-        if (prev_idx != -1 && memo[idx][prev_idx] != -1) 
-            return memo[idx][prev_idx];
-
-        // Skip the current element
-        int skip = a1(nums, idx + 1, prev_idx);
-
-        // Take the current element if it is larger than the previous one
-        int take = 0;
-        if (prev_idx == -1 || nums[idx] > nums[prev_idx]) {
-            take = 1 + a1(nums, idx + 1, idx);
-        }
-
-        if (prev_idx != -1)
-            memo[idx][prev_idx] = max(skip, take);
-
-        return max(skip, take);
-    }
-
-    int a2(vector<int> &nums){
+    int solve(vector<int> &nums,int idx ,int prev_idx){
+        
         int n=nums.size();
-
-        vector<int> dp(n,1);
-        int maxLIS=1;
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-
-                if(nums[j]<nums[i]){
-                    dp[i]=max(dp[i],dp[j]+1);
-                    maxLIS=max(maxLIS,dp[i]);
-                }
-            }
+        if(idx>=n){
+            return 0;
         }
-        return maxLIS;
+
+        if( prev_idx!=-1 && memo[idx][prev_idx]!=-1) return memo[idx][prev_idx];
+
+        int skip=solve(nums,idx+1,prev_idx);
+
+        int take=0;
+        if(prev_idx==-1 || nums[idx]>nums[prev_idx]){
+            take =1+solve(nums,idx+1,idx);
+        }
+
+        if(prev_idx!=-1)
+        return memo[idx][prev_idx]=max(skip,take);
+
+        return max(skip,take);
 
     }
     int lengthOfLIS(vector<int>& nums) {
-
-        // APPROACH 1:- USING RECURSION+MEMOISATION
-        // FOR RECURSION :- T.C:- O(2^N)
-        // FOR MEMOIZATION T.C :- O(N^2) , S.C :- O(N^2);
-        memset(memo, -1, sizeof(memo));
-
-        // return a1(nums, 0, -1);
-
-        // // APPROACH 2:- USING DYNAMIC PROGRAMMING
-        // FOR DYNAMIC PROGRAMMING T.C :- O(N^2) , S.C :- O(N^2);
-
-        return a2(nums);
-
+        
+        memset(memo,-1,sizeof(memo));
+        return solve(nums,0,-1);
     }
 };
