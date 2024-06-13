de6e36bb898faba1d4e53598e5c67e95f10d27df
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-06-13 13:53:04 +0530

    Time: 14 ms (6.67%), Space: 21 MB (17.35%) - LeetHub

--- 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        
+    int a1(vector<int>& seats, vector<int>& students){
+
         int n=seats.size();
         sort(seats.begin(),seats.end());
         sort(students.begin(),students.end());
@@ -13,5 +13,47 @@ public:
         }
         
         return cnt;
+    }
+
+    int a2(vector<int>& seats, vector<int>& students){
+
+        int n=seats.size();
+
+        vector<int> position_seats(101,0);
+        vector<int> position_stud(101,0);
+
+        for(auto &x : seats) position_seats[x]++;
+        for(auto &x : students) position_stud[x]++;
+
+        int i=0,j=0;
+        int moves=0;
+
+        while(i<=100 && j<=100){
+
+            if(position_seats[i]==0) i++;
+            if(position_stud[j]==0) j++;
+
+            if(i<=100 && j<=100 && position_seats[i]!=0 && position_stud[j]!=0){
+
+                moves+=abs(i-j);
+                position_seats[i]--;
+                position_stud[j]--;
+                
+            }
+        }
+
+        return moves;
+
+    }
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+    
+    // APPROACH 1:- USING SORTING 
+    // T.C:- O(nlogn) , S.C :- O(1)
+        // return a1(seats,students);
+
+    // APPROACH 2:- USING COUNT SORT
+    // T.C :- O(N) , S.C :- O(N)
+        return a2(seats,students);
+
     }
 };
