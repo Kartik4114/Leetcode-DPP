66000cc22d22bb1644adec4f1fc8eb36f9a05a96
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-06-14 17:12:03 +0530

    Time: 30 ms (39.51%), Space: 30 MB (8.71%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -18,13 +18,38 @@ public:
         return memo[i][j]=max(take,skip);
 
     }
+
+    int a2(string s1,string s2){
+        int m=s1.size();
+        int n=s2.size();
+        vector<vector<int>> dp(m+1,vector<int> (n+1,0));
+
+        for(int row=0;row<=m;row++){
+            dp[row][0]=0;
+        }
+        for(int col=0;col<=n;col++){
+            dp[0][col]=0;
+        }
+
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                } else {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
     int longestCommonSubsequence(string text1, string text2) {
         
         // APPROACH 1:- USING RECURSION +MEMOIZATION
         memset(memo,-1,sizeof(memo));
-        return a1(text1,text2,0,0);
+        // return a1(text1,text2,0,0);
 
-        // approach 2:- using S
+        // approach 2:- using dp
+        return a2(text1,text2);
 
     }
 };
