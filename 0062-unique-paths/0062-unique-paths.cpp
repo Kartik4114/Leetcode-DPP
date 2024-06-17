24f8763a8ae3dba737beff0324594fac6c693481
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-06-17 17:23:36 +0530

    Time: 0 ms (100%), Space: 7.1 MB (86.46%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,32 +3,31 @@ public:
     int cnt;
     
     int memo[101][101];
-    int helper(vector<vector<int>> &board,int m,int n,int i,int j){
+    int helper(int m,int n,int i,int j){
         if(i==m-1 && j==n-1) {
             // cnt++;
             return 1;
         }
 
         if(memo[i][j]!=-1) return memo[i][j];
-        if(i>=m || i<0 || j>=n || j<0 || board[i][j]==1) return 0;
-
-        board[i][j]=1;
+        if(i>=m || i<0 || j>=n || j<0) return 0;
 
             int ans=0;
-            ans+= helper(board,m,n,i+1,j);
-            ans+= helper(board,m,n,i,j+1);
-        
+            ans+= helper(m,n,i+1,j);
+            ans+= helper(m,n,i,j+1);
 
-        board[i][j]=0;
         return memo[i][j]=ans;
     }
     int uniquePaths(int m, int n) {
         
-        
-        vector<vector<int>> board(m,vector<int>(n,0));
+        // T.C :- FOR RECURSION O(2^(M*N))
+        // S.C :- O(STACK SPACE), O(PATH_LENGTH) , O((M-1)*(N-1));
+
+        // T.C :- FOR MEMOIZZATION O(M*N)
+        // S.C :- O(STACK SPACE) i.e O((M-1)*(N-1)) + O(M*N);
         memset(memo,-1,sizeof(memo));
 
-        return helper(board,m,n,0,0);
+        return helper(m,n,0,0);
         
     }
 };
