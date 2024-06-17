6964e24ff0153ed16c589d9b391e6d4eec0274b3
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-06-17 09:00:11 +0530

    Time: 0 ms (100%), Space: 7 MB (61.35%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    bool judgeSquareSum(int c) {
-        
+    bool a1(int c){
         for(long long i=0;i*i<=c;i++){
 
             long long a=i*i;
@@ -13,5 +12,31 @@ public:
         }
 
         return false;
+    }
+    
+    bool a2(int c){
+
+        long long left=0;
+        long long right= sqrt(c);
+
+        while(left<=right){
+
+            long currSum=left*left+right*right;
+
+            if(currSum==c) return true;
+            if(currSum<c) left++;
+            else right--;
+        }
+
+        return false;
+    }
+    bool judgeSquareSum(int c) {
+        
+        // approach 1:- using maths
+        // return a1(c);
+
+        // APPROACH 2:- USING BINARY SEARCH
+        return a2(c);
+        
     }
 };
