397693cc788b5f0f428a08e5e37db78d60f16087
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-06-17 23:19:58 +0530

    Time: 3 ms (33%), Space: 7.1 MB (95.38%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    int cnt;
-    
     int memo[101][101];
-    int helper(int m,int n,int i,int j){
-        if(i==m-1 && j==n-1) {
-            // cnt++;
-            return 1;
+    
+int helper(int i, int j) {
+    if (i < 0 || j < 0) return 0;
+    if (i == 0 && j == 0) return 1;
+    if (memo[i][j] != -1) return memo[i][j];
+
+    int ans = 0;
+    ans += helper(i - 1, j);
+    ans += helper(i, j - 1);
+
+    return memo[i][j] = ans;
+}
+
+    int a3(int m,int n){
+
+        int dp[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=1;
+                else {
+                    int up=0;
+                    int left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+
+                    dp[i][j]=up+left;
+                }
+            }
         }
-
-        if(memo[i][j]!=-1) return memo[i][j];
-        if(i>=m || i<0 || j>=n || j<0) return 0;
-
-            int ans=0;
-            ans+= helper(m,n,i+1,j);
-            ans+= helper(m,n,i,j+1);
-
-        return memo[i][j]=ans;
+        return dp[m-1][n-1];
     }
     int uniquePaths(int m, int n) {
         
@@ -25,9 +39,15 @@ public:
 
         // T.C :- FOR MEMOIZZATION O(M*N)
         // S.C :- O(STACK SPACE) i.e O((M-1)*(N-1)) + O(M*N);
-        memset(memo,-1,sizeof(memo));
+        // memset(memo,-1,sizeof(memo));
+        // return helper(m-1,n-1);
 
-        return helper(m,n,0,0);
+        // approach 3:- using dynamic programming
+        // T.C:- O(M*N), S.C :- O(M*N)
+        return a3(m,n);
+
+
+        
         
     }
 };
