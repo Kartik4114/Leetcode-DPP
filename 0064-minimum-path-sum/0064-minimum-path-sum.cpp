8be766577f55b4024026b41ca14a2b11f1a87bc2
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-06-22 00:11:55 +0530

    Time: 3 ms (95.08%), Space: 12.8 MB (12.51%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -13,6 +13,34 @@ public:
 
         return memo[i][j]=min(up,left);
     }
+
+    int a2(vector<vector<int>> &grid){
+        
+        int m=grid.size();
+        int n=grid[0].size();
+
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=grid[i][j];
+
+                else if(i==0){
+                    dp[i][j]=grid[i][j]+dp[i][j-1];
+                }
+                
+                else if(j==0) {
+                    dp[i][j]=grid[i][j]+dp[i-1][j];
+                }
+
+                else {
+                    dp[i][j]=grid[i][j] +min(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+
+    }
     int minPathSum(vector<vector<int>>& grid) {
     
     // APPROACH 1:- USING RECURSION +MEMO
@@ -20,6 +48,9 @@ public:
         int m=grid[0].size();
 
         memset(memo,-1,sizeof(memo));
-        return a1(grid,n-1,m-1);
+        // return a1(grid,n-1,m-1);
+
+        // APPROACH 2:- USING DYNAMIC PROGRAMMING
+        return a2(grid);
     }
 };
