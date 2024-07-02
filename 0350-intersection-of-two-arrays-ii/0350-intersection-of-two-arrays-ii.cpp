3131aa01b3288eb047bcf994556d4c055572545c
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-02 10:32:35 +0530

    Time: 9 ms (11.77%), Space: 14.1 MB (25.45%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        
+    vector<int> a1(vector<int>& nums1, vector<int>& nums2){
         int n=nums1.size();
         int m=nums2.size();
         vector<int> result;
@@ -23,5 +22,34 @@ public:
         }
 
         return result;
+    }
+
+    vector<int> a2(vector<int>& nums1, vector<int>& nums2){
+        unordered_map<int,int> mp;
+        vector<int> result;
+
+        for(auto num:nums1){
+            mp[num]++;
+        }
+
+        for(auto num:nums2){
+            if(mp[num]>0){
+                result.push_back(num);
+                mp[num]--;
+            }
+        }
+        return result;
+    }
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        
+        // APPROACH 1:- USING SORTING AND TRAVERSING (BEST APPROACH)
+        // T.C:- O(nlogn) ,S.C :- O(1)
+        // return a1(nums1,nums2);
+
+        // APPROACH 2:- USING MAP
+        // T.C:- O(n) ,S.C :- O(n)
+        return a2(nums1,nums2);
+
+
     }
 };
