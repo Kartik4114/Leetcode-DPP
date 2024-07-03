f7a38a3ad061260a7130983a3afb57d9fd12924d
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-04 01:06:27 +0530

    Time: 69 ms (55.06%), Space: 37.7 MB (94.07%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -16,9 +16,35 @@ public:
         return result;
 
     }
+
+    int a2(vector<int> &nums){
+        
+        int n=nums.size();
+        if(n<=4) return 0;
+        // We have to remember the syntax of partial sort and nth element
+        partial_sort(nums.begin(),nums.begin()+4,nums.end()); // O(nlog(4)) =O(n)
+
+        nth_element(nums.begin()+4,nums.begin()+n-4,nums.end()); // O(n)
+
+        sort(nums.begin()+n-4,nums.end()); // O(4log4) ==O(1)
+
+        int result=INT_MAX;
+         result=min(result,nums[n-4]-nums[0]);
+        result=min(result,nums[n-1]-nums[3]);
+        result=min(result,nums[n-2]-nums[2]);
+        result=min(result,nums[n-3]-nums[1]);
+
+        return result;
+    }
     int minDifference(vector<int>& nums) {
         
         // APPROACH 1:- USING SORTING
-        return a1(nums);
+        // T.C :- O(nlogn) , S.C :- O(1)
+        // return a1(nums);
+
+        // APPROACH 2:- USING PARTIAL SORT AND NTH ELEMENT (MORE OPTIMISED APPROACH)
+        // T.C :- O(n) , S.C :- O(1)
+        return a2(nums);
+
     }
 };
