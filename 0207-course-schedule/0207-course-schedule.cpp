4f070a6101d91f251aef521f109a57d7914438e1
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-09 17:05:10 +0530

    Time: 14 ms (74.44%), Space: 19 MB (13.03%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -42,12 +42,53 @@ public:
 
         return BFS(numCourses,adj,inDegree);
     }
+    
+    bool isCycleDFS(unordered_map<int,vector<int>> &adj,vector<bool> &visited,int u,
+                    vector<bool> &inRecursion){
+        visited[u]=true;
+        inRecursion[u]=true;
+
+        for(auto &v:adj[u]){
+            if(!visited[v] && isCycleDFS(adj,visited,v,inRecursion))
+            return true;
+
+            else if(inRecursion[v]==true) return true;
+        }
+        inRecursion[u]=false;
+        return false;
+
+    }
+    bool a2(int numCourses, vector<vector<int>>& prerequisites){
+        unordered_map<int,vector<int>> adj;
+
+        vector<bool> visited(numCourses,false);
+        vector<bool> inRecursion(numCourses,false);
+
+        for(auto &vec:prerequisites){
+            int a=vec[0];
+            int b=vec[1];
+
+            adj[b].push_back(a);
+        }
+
+        for(int i=0;i<numCourses;i++){
+
+            if(!visited[i] && isCycleDFS(adj,visited,i,inRecursion))
+             return false;
+        }
+        return true;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         
         // APPROACH 1:- USING BFS
         // IT IS DETECT CYCLE IN DIRECTED GRAPH(KAHN'S ALGORITHM)
 
-        return a1(numCourses,prerequisites);
+        // return a1(numCourses,prerequisites);
+
+        // APPROACH 2:- USING DFS
+
+        // IT IS DETECT CYCLE IN DIRECTED GRAPH
+        return a2(numCourses,prerequisites);
 
     }
 };
