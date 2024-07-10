b55010b942496e7bb15a56ca506ec5c58260de8e
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-10 12:20:13 +0530

    Time: 5 ms (46.3%), Space: 14.2 MB (31.02%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.cpp
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -1,18 +1,46 @@
 class Solution {
 public:
-    int minOperations(vector<string>& logs) {
-        
+    int a1(vector<string>& logs){
+
         int cnt=0;
         int n=logs.size();
         for(int i=0;i<n;i++){
             if(logs[i]=="../"){
-                if(cnt>0) cnt--;
-                else continue;
+               cnt=max(cnt,cnt-1);
             } else if(logs[i]=="./") continue;
 
             else cnt++;
         }
 
         return cnt;
+    }
+
+    int a2(vector<string> &logs){
+        stack<string> st;
+        int n=logs.size();
+
+        for(int i=0;i<n;i++){
+
+            if(logs[i]=="../" ){
+                if(!st.empty()){
+                    st.pop();
+                }
+            } else if(logs[i] == "./") continue;
+            else st.push(logs[i]);
+
+        }
+
+        return st.size();
+
+    }
+    int minOperations(vector<string>& logs) {
+        
+        // APPROACH 1:- USING SIMPLE TRAVERSE THE ARRAY
+        // T.C:- O(n) , S.C :- O(1)
+        // return a1(logs);
+
+        // APPROACH 2:- USING STACK
+        // T.C:- O(n) , S.C :- O(n)
+        return a2(logs);
     }
 };
