b98c1e46299cfaba929fc71021290dac13707758
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-10 13:16:34 +0530

    Time: 23 ms (19.24%), Space: 16.4 MB (27.52%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -13,8 +13,8 @@ public:
         }
         return true;
     }
-    bool isBipartite(vector<vector<int>>& graph) {
-        
+
+    bool a1(vector<vector<int>>& graph){
         int n=graph.size();
         vector<int> color(n,-1);
 
@@ -24,5 +24,50 @@ public:
             }
         }
         return true;
+    }
+
+    bool checkBipartiteBFS(vector<vector<int>>& graph,int curr,vector<int> &color,int currColor){
+        color[curr]=currColor;
+
+        queue<int> que;
+        que.push(curr);
+
+        while(!que.empty()){
+            int front=que.front();
+            que.pop();
+
+            for(auto &v:graph[front]){
+                if(color[v]==color[front]) return false;
+
+                else if(color[v]==-1){
+                    color[v]=1-color[front];
+                    que.push(v);
+                }
+            }
+        }
+        return true;
+
+    }
+    bool a2(vector<vector<int>>& graph){
+
+        int n=graph.size();
+        vector<int> color(n,-1);
+
+        // RED 1
+        // GREEn 0
+        for(int i=0;i<n;i++){
+            
+            if(color[i]==-1)
+            if(checkBipartiteBFS(graph,i,color,1)==false) return false;
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        
+        // APPROACH 1:- USING DFS
+        // return a1(graph);
+
+        // APPROACH 1:- USING BFS
+        return a2(graph);
     }
 };
