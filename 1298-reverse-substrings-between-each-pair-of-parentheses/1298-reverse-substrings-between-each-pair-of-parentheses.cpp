2d0e45ef1fb8401c60a24edc8106b4a152b9bfe2
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-11 12:16:02 +0530

    Time: 4 ms (27.22%), Space: 8.3 MB (37.54%) - LeetHub

---
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        
+        stack<int> st;
+        queue<int> que;
+
+        int n=s.size();
+
+        for(int i=0;i<n;i++){
+
+            if(s[i]!=')'){
+                st.push(s[i]);
+            }
+            else{
+                while(!st.empty() && st.top()!='('){
+                    que.push(st.top());
+                    st.pop();
+                }
+                st.pop();
+
+                    while(!que.empty()){
+                        st.push(que.front());
+                        que.pop();
+                    }
+            }
+        }
+
+        // string result;
+        // while(!que.empty()){
+        //     result.push_back(que.front());
+        //     que.pop();
+        // }
+
+        string result;
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
