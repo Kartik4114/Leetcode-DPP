699803dc4bb306cb12d5a281b8f24c66ba1a44ab
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-12 23:29:20 +0530

    Time: 71 ms (77.64%), Space: 24.1 MB (49.59%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string removeSubstr(string &s,string &matchStr){
+    string removeSubstr_a1(string &s,string &matchStr){
 
         stack<char> st;
         for(auto ch:s){
@@ -27,13 +27,54 @@ public:
 
         // first pass
 
-        string temp_first=removeSubstr(s,maxStr);
+        string temp_first=removeSubstr_a1(s,maxStr);
         int L=temp_first.length();
 
         int charRemoved=n-L;
         score+=(charRemoved/2)*max(x,y);
 
-        string temp_second=removeSubstr(temp_first,minStr);
+        string temp_second=removeSubstr_a1(temp_first,minStr);
+        charRemoved=L-temp_second.length();
+        
+        score+=(charRemoved/2)*min(x,y);
+
+        return score;
+    }
+
+    string removeSubstr_a2(string &s,string &matchStr){
+        
+        int n=s.size();
+        int i=0; // used for writing purpose
+
+        for(int j=0;j<n;j++){
+            s[i]=s[j];
+            i++;
+
+            if(i>=2 && s[i-2]==matchStr[0] && s[i-1]==matchStr[1]){
+                i-=2;
+            }
+        }
+
+        s.erase(s.begin()+i,s.end());
+        return s;
+    }
+
+    int a2(string s,int x,int y){
+        int n=s.length();
+        int score=0;
+
+        string maxStr= (x>y) ? "ab" : "ba";
+        string minStr= (x<y) ? "ab" : "ba";
+
+        // first pass
+
+        string temp_first=removeSubstr_a2(s,maxStr);
+        int L=temp_first.length();
+
+        int charRemoved=n-L;
+        score+=(charRemoved/2)*max(x,y);
+
+        string temp_second=removeSubstr_a2(temp_first,minStr);
         charRemoved=L-temp_second.length();
         
         score+=(charRemoved/2)*min(x,y);
@@ -43,6 +84,12 @@ public:
     int maximumGain(string s, int x, int y) {
         
         // APPROACH 1:- USING STACKS
-        return a1(s,x,y);
+        // T.C :- O(N) , S.C :- O(N)
+        // return a1(s,x,y);
+
+         // APPROACH 2:- USING TWO POINTERS (MOST OPTIMISED APPROACH)
+        // T.C :- O(N) , S.C :- O(N)
+        return a2(s,x,y);
+
     }
 };
