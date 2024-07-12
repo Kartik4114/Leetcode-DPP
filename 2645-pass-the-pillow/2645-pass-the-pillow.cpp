19ee5dc364cc2361d20d2e157655bf7c2cd30a56
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-12 14:13:05 +0530

    Time: 0 ms (100%), Space: 7.5 MB (38.21%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
+    int a1(int n,int time){
+
+        int idx=1;
+        int dir=1;
+
+        while(time>0){
+            if(idx+dir>=1 && idx+dir<=n){
+                idx+=dir;
+                time--;
+            } else dir=-dir;
+        }
+        return idx;
+    }
     int passThePillow(int n, int time) {
         
-        int travel=time/(n-1);
-
-        if(travel%2==0){
-            return time%(n-1)+1;
-        } else return n- time%(n-1);
-
+        // APPROACH 1:- DO AS QUESTION SAY
+        // T.C :- O(n) 
+        return a1(n,time);
     }
 };
