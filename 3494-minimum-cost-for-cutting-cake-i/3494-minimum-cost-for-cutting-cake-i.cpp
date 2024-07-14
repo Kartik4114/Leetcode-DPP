d0516517223302796fe1f7f0d065951cd3628e56
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-14 17:31:09 +0530

    Time: 8 ms (62.5%), Space: 29.4 MB (100%) - LeetHub

---
+++ 3494-minimum-cost-for-cutting-cake-i/3494-minimum-cost-for-cutting-cake-i.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
+        
+        int x=horizontalCut.size();
+        int y=verticalCut.size();
+
+        sort(begin(horizontalCut), end(horizontalCut), greater<int>());
+        sort(begin(verticalCut), end(verticalCut), greater<int>());
+
+        int i=0;
+        int j=0;
+
+        int horizontalPieces=1;
+        int verticalPieces=1;
+
+        int totalCost=0;
+        while(i<x && j<y){
+            if(horizontalCut[i]>=verticalCut[j]){
+                totalCost+=horizontalCut[i]*verticalPieces;
+                horizontalPieces++;
+                i++;
+            } else {
+                totalCost+=verticalCut[j]*horizontalPieces;
+                verticalPieces++;
+                j++;
+            }
+        }
+
+        while(i<x){
+            totalCost+=horizontalCut[i]*verticalPieces;
+            horizontalPieces++;
+            i++;
+        }
+        while(j<y){
+            totalCost+=verticalCut[j]*horizontalPieces;
+            verticalPieces++;
+            j++;
+        }
+
+        return totalCost;
+    }
+};
