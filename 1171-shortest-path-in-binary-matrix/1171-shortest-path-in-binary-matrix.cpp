49a3a796ac40904c5c0a58167d5eb63fe490b467
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-17 01:05:48 +0530

    Time: 47 ms (57.95%), Space: 26.3 MB (22.73%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -122,6 +122,51 @@ public:
         return result[m-1][n-1]+1;
 
     }
+
+    int a4(vector<vector<int>> &grid){
+
+        int m=grid.size();
+        int n=grid[0].size();
+
+        if(m==0 || n==0 || grid[0][0]!=0) return -1;
+
+        auto isSafe=[&](int x,int y){
+            return x>=0 && y>=0 && x<m && y<n;
+        };
+
+        queue<P> pq;
+        vector<vector<int>> result(m,vector<int>(n,INT_MAX));
+
+        pq.push({0,{0,0}});
+        result[0][0]=0;
+
+        while(!pq.empty()){
+
+            int d=pq.front().first;
+            auto node=pq.front().second;
+            int x=node.first;
+            int y=node.second;
+
+            pq.pop();
+
+            for(auto &dir:directions){
+
+                int new_x=x+dir[0];
+                int new_y=y+dir[1];
+
+                int dist=1;
+
+                if(isSafe(new_x,new_y) && grid[new_x][new_y]==0 && d+dist<result[new_x][new_y]){
+                    pq.push({d+dist,{new_x,new_y}});
+                    result[new_x][new_y]=d+dist;
+                }
+            }
+        }
+
+        if(result[m-1][n-1]==INT_MAX) return -1;
+        return result[m-1][n-1]+1;
+
+    }
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
 
         // REMEMBER IF YOU TRYING TO USE HERE RECURSION THEN IT WILL GIVE TLE
@@ -140,6 +185,10 @@ public:
         // return a2(grid);
 
         // APPROACH 3:- USING DIJKSTRA'S ALGORITHM (using min heap in this)
-        return a3(grid);
+        // return a3(grid);
+
+        // APPROACH 4:- USING DIJKSTRA'S ALGORITHM (using queue in this)
+        // bec. distance is same which is 1
+        return a4(grid);
     }
 };
