757f1406dfd39e2ff841e274d536f053fdb815f9
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-19 23:53:32 +0530

    Time: 20 ms (23.27%), Space: 14.3 MB (27.66%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
-        
+    vector<int> a1(vector<vector<int>>& matrix){
         int m=matrix.size();
         int n=matrix[0].size();
 
@@ -29,5 +28,47 @@ public:
         }
 
         return result;
+    }
+
+    vector<int> a2(vector<vector<int>>& matrix){
+
+        int m=matrix.size();
+        int n=matrix[0].size();
+
+        int rMinMax=-1;
+        for(int row=0;row<m;row++){
+            
+            int rMin=INT_MAX;
+            for(int col=0;col<n;col++){
+                rMin=min(rMin,matrix[row][col]);
+            }
+
+            rMinMax=max(rMinMax,rMin);  
+        }
+
+        int cMaxMin=INT_MAX;
+        for(int col=0;col<n;col++){
+
+            int cMax=-1;
+            for(int row=0;row<m;row++){
+                cMax=max(cMax,matrix[row][col]);
+            }
+
+            cMaxMin=min(cMaxMin,cMax);
+        }
+
+        if(rMinMax==cMaxMin)  return {rMinMax};
+        return {};
+    }
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        
+        // APPROACH 1:-
+        // T.C :- O(N^2) , S.C :- O(n) 
+        // return a1(matrix);
+
+        // APPROACH 2:-  OPTIMISED APPROACH
+        // here we are using the fact that there will be only one lucky number
+        // T.C :- O(N^2) , S.C :-O(1)
+        return a2(matrix);
     }
 };
