ef7088fecb524627c9b8af9fa11b7a36a3eea869
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-22 12:50:05 +0530

    Time: 103 ms (54.44%), Space: 61.5 MB (72.22%) - LeetHub

--- 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.cpp
+++ 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.cpp
@@ -1,67 +1,76 @@
 class Solution {
 public:
+    void dfs(vector<int> &visited,unordered_map<int,vector<int>> &adj,stack<int> &st,int u,bool &cycle){
+
+        visited[u]=1;
+        for(auto v:adj[u]){
+
+            if(visited[v]==0){
+                dfs(visited,adj,st,v,cycle);
+            } else if(visited[v]==1){
+                cycle=true;
+                return;
+            }
+        }
+
+        visited[u]=2;
+        st.push(u);
+    }
+    vector<int> topoSort(vector<vector<int>> &edges,int k){
+
+        unordered_map<int,vector<int>> adj;
+
+        for(auto &edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+
+            // u->v
+            adj[u].push_back(v);
+        }
+        vector<int> visited(k+1,0);
+        // 0 -> not visited
+        // 1 -> in recursion or in process
+        // 2 -> already visited
+        
+        stack<int> st;
+        bool cycle=false;
+        for(int i=1;i<=k;i++){
+            
+            if(!visited[i]){
+                dfs(visited,adj,st,i,cycle);
+                if(cycle){
+                    return {};
+                }
+            }
+        }
+
+        vector<int> order; // final topological sorted array
+
+        while(!st.empty()){
+            order.push_back(st.top());
+            st.pop();
+        }
+        return order;
+    }
     vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
-        unordered_map<int, vector<int>> graph;
-        for (const auto& edge : rowConditions) {
-            graph[edge[0]].push_back(edge[1]);
+        
+        vector<int> topoRow=topoSort(rowConditions,k);
+        vector<int> topoCol=topoSort(colConditions,k);
+
+        if(topoRow.empty() || topoCol.empty()) return {};
+
+        vector<vector<int>> matrix(k,vector<int>(k,0));
+
+        for(int i=0;i<k;i++){
+            for(int j=0;j<k;j++){
+
+                if(topoRow[i]==topoCol[j]){
+                    matrix[i][j]=topoRow[i];
+                }
+            }
         }
 
-        vector<int> row_sorting = topo_sort(graph, k);
-        graph.clear();
-        for (const auto& edge : colConditions) {
-            graph[edge[0]].push_back(edge[1]);
-        }
-        vector<int> col_sorting = topo_sort(graph, k);
+        return matrix;
 
-        if (row_sorting.empty() || col_sorting.empty()) {
-            return {};
-        }
-
-        unordered_map<int, pair<int, int>> value_position;
-        for (int i = 0; i < k; ++i) {
-            value_position[row_sorting[i]].first = i;
-            value_position[col_sorting[i]].second = i;
-        }
-
-        vector<vector<int>> res(k, vector<int>(k, 0));
-        for (int value = 1; value <= k; ++value) {
-            int row = value_position[value].first;
-            int column = value_position[value].second;
-            res[row][column] = value;
-        }
-
-        return res;
-    }
-
-private:
-    bool dfs(int src, unordered_map<int, vector<int>>& graph, unordered_set<int>& visited, unordered_set<int>& cur_path, vector<int>& res) {
-        if (cur_path.count(src)) return false;  // cycle detected
-        if (visited.count(src)) return true;  // all okay, but we've already visited this node
-
-        visited.insert(src);
-        cur_path.insert(src);
-
-        for (int neighbor : graph[src]) {
-            if (!dfs(neighbor, graph, visited, cur_path, res))  // if any child returns false
-                return false;
-        }
-
-        cur_path.erase(src);  // backtrack path
-        res.push_back(src);
-        return true;
-    }
-
-    vector<int> topo_sort(unordered_map<int, vector<int>>& graph, int k) {
-        unordered_set<int> visited;
-        unordered_set<int> cur_path;
-        vector<int> res;
-
-        for (int src = 1; src <= k; ++src) {
-            if (!dfs(src, graph, visited, cur_path, res))
-                return {};
-        }
-
-        reverse(res.begin(), res.end());  // we will have res as reversed so we need to reverse it one more time
-        return res;
     }
 };
