aee3cdbb8ed79c4af701c0f61b10fe1e043dfdce
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-22 23:29:31 +0530

    Time: 3 ms (41.17%), Space: 8.5 MB (9.37%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        
+        unordered_set<string> bankSet(bank.begin(),bank.end());
+        unordered_set<string> visited;
+
+        queue<string> que;
+        int level=0;
+        que.push(startGene);
+        visited.insert(startGene);
+
+        while(!que.empty()){
+
+            int n=que.size();
+            while(n--){
+
+                string curr=que.front();
+                que.pop();
+
+                if(curr==endGene){
+                    return level;
+                }
+
+                for(auto &ch:"ACGT"){
+                    for(int i=0;i<curr.length();i++){
+
+                        string neighbor=curr;
+                        neighbor[i]=ch;
+                        if(visited.find(neighbor)==visited.end() && bankSet.find(neighbor)!=bankSet.end()){
+                            visited.insert(neighbor);
+                            que.push(neighbor);
+                        }
+                    }
+                }
+            }
+            level++;
+        }
+        return -1;
+    }
+};
