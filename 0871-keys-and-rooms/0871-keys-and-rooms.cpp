9b53abc1af99224134bf04692a8e925983ccb9bd
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-23 14:04:06 +0530

    Time: 3 ms (94.97%), Space: 13.9 MB (37.48%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.cpp
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -11,12 +11,32 @@ public:
             }
         }
     }
+
+    void bfs(vector<vector<int>> &rooms,vector<bool> &visited){
+
+        queue<int> que;
+        visited[0]=true;
+        que.push(0);
+
+        while(!que.empty()){
+            auto front=que.front();
+            que.pop();
+
+            for(auto &v: rooms[front]){
+                if(!visited[v]){
+                    visited[v]=true;
+                    que.push(v);
+                }
+            }
+        }
+    }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
 
         int n=rooms.size();
         vector<bool> visited(n,false);
 
-        dfs(rooms,visited,0);
+        // dfs(rooms,visited,0);
+        bfs(rooms,visited);
 
         for(int i=0;i<n;i++){
             if(visited[i]==false){
