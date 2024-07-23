13f603f84db41662574dc080f1b272afeb02c52a
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-23 13:47:20 +0530

    Time: 3 ms (94.97%), Space: 13.7 MB (58.1%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.cpp
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    void dfs(unordered_map<int,vector<int>> &adj,vector<bool> &visited,int u){
+    void dfs(vector<vector<int>> &adj,vector<bool> &visited,int u){
         visited[u]=true;
 
         for(auto &v:adj[u]){
@@ -12,18 +12,11 @@ public:
         }
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        
+
         int n=rooms.size();
-        unordered_map<int,vector<int>> adj;
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<rooms[i].size();j++)
-            adj[i].push_back(rooms[i][j]);
-        }
-
         vector<bool> visited(n,false);
 
-        dfs(adj,visited,0);
+        dfs(rooms,visited,0);
 
         for(int i=0;i<n;i++){
             if(visited[i]==false){
