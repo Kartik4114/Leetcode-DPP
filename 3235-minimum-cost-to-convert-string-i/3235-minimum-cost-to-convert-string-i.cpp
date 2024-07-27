0de6f37aa537b7202a857a3bf7ccb07c7196dab6
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-27 14:10:08 +0530

    Time: 189 ms (46.1%), Space: 103.1 MB (42.8%) - LeetHub

--- 3235-minimum-cost-to-convert-string-i/3235-minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/3235-minimum-cost-to-convert-string-i.cpp
@@ -36,9 +36,71 @@ public:
 
         return ans;
     }
+
+    void dijkstra(vector<vector<long long>> &costMatrix,unordered_map<char,vector<pair<char,int>>> &adj,int source){
+
+        priority_queue<pair<int,char>,vector<pair<int,char>>,greater<pair<int,char>>> pq;
+
+        pq.push({0,source});
+
+        while(!pq.empty()){
+
+            int d=pq.top().first;
+            char adjNode=pq.top().second;
+            pq.pop();
+
+            for(auto &it: adj[adjNode]){
+
+                char neighbor=it.first;
+                int cost = it.second;
+
+                if(costMatrix[source-'a'][neighbor-'a']>d+cost){
+                    costMatrix[source-'a'][neighbor-'a']=d+cost;
+                    pq.push({d+cost,neighbor});
+                }
+            }
+        }
+    }
+    long long a2(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost){
+        
+        int n=original.size();
+        unordered_map<char,vector<pair<char,int>>> adj;
+
+        for(int i=0;i<n;i++){
+            adj[original[i]].push_back({changed[i],cost[i]});
+        }
+
+        vector<vector<long long>> costMatrix(26,vector<long long>(26,INT_MAX));
+
+        for(int i=0;i<source.size();i++){
+            char ch=source[i];
+            dijkstra(costMatrix,adj,ch);
+        }
+
+        long long ans=0;
+        for(int i=0;i<source.length();i++){
+            if(source[i]==target[i]) continue;
+
+            if(costMatrix[source[i]-'a'][target[i]-'a']==INT_MAX) return -1;
+
+            ans+=costMatrix[source[i]-'a'][target[i]-'a'];
+        }
+
+        return ans;
+    }
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
         
         // APPROACH 1:- USING FLOYD WARSHALL ALGORITHM
+        //T.C : O(n)  -> Because other for loops run only for constant time 26*26*26
+        //S.C : O(1) -> We take distances matrix of 26*26 which is constant 
         return a1(source,target,original,changed,cost);
+
+        //Approach-2 (Using Simple Dijkstra's Algorithm)
+        //T.C : n * (time complexity of Dijkstra) = O(n * (V+E)logV) ,
+        //  where V = number of vertices and E = number of edges
+        //S.C : O(V+E) -> We create a graph, where V = number of vertices and E = number of edges
+        return a2(source,target,original,changed,cost);
+
+
     }
 };
