dd297c45743dbee1f25373064c3a07660980cd0d
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-30 00:42:55 +0530

    Time: 115 ms (83.7%), Space: 103.6 MB (70.53%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
@@ -40,8 +40,66 @@ public:
 
         return minDistNode;
     }
+
+    void BFS(vector<int> &edges,vector<int> &dist,int node){
+        
+        int n=edges.size();
+        vector<bool> visited(n,false);
+
+        queue<int> que;
+        que.push(node);
+        visited[node]=true;
+
+        while(!que.empty()){
+
+            int front=que.front();
+            que.pop();
+
+            int v=edges[front];
+            if(v!=-1 && !visited[v]){
+                dist[v]=1+dist[front];
+                que.push(v);
+                visited[v]=true;
+
+            }
+        }
+    }
+    int a2(vector<int>& edges, int node1, int node2) {
+        
+        int n=edges.size();
+        vector<int> dist1(n,INT_MAX);
+        vector<int> dist2(n,INT_MAX);
+
+        dist1[node1]=0;
+        dist2[node2]=0;
+        
+        BFS(edges,dist1,node1);
+        BFS(edges,dist2,node2);
+
+        int minDistNode=-1;
+        int minDistTillNow=INT_MAX;
+        
+        for(int i=0;i<n;i++){
+
+            int maxD=max(dist1[i],dist2[i]);
+
+            if(minDistTillNow>maxD){
+                minDistTillNow=maxD;
+                minDistNode=i;
+            }
+        }
+
+        return minDistNode;
+    }
+
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         
-        return a1(edges,node1,node2);
+        // APPROACH 1:- USING DFS 
+        // T.C :- O(2*n) bec as their is only one edge from each node so visiting only once
+        // return a1(edges,node1,node2);
+
+        // APPROACH 2:- USING BFS 
+        // T.C :- O(2*n) bec as their is only one edge from each node so visiting only once
+        return a2(edges,node1,node2);
     }
 };
