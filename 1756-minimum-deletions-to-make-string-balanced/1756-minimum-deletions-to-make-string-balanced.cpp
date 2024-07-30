8c28c3d4ae0e120ac5d9e246a16ac1ffcdc359a6
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-07-30 23:18:13 +0530

    Time: 86 ms (66.83%), Space: 27.5 MB (49.44%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -15,10 +15,42 @@ public:
         }
         return cnt;
     }
+
+    int a2(string s){
+
+        int n = s.size();
+
+    if (n == 0) return 0; // Handle empty string case
+
+    int cntRightOfA = 0;
+    for (int i = 0; i < n; ++i) {
+        if (s[i] == 'a') cntRightOfA++;
+    }
+
+    if (cntRightOfA == 0 || cntRightOfA == n - 1) return 0;
+
+    int minCnt = INT_MAX;
+    int cntOfBLeft = 0;
+    for (int i = 0; i < n; ++i) {
+        if (s[i] == 'a') cntRightOfA--;
+        
+        int toDelete = cntRightOfA + cntOfBLeft;
+        minCnt = min(minCnt, toDelete);
+        if (s[i] == 'b') cntOfBLeft++;
+    }
+
+    return minCnt;
+    }
     int minimumDeletions(string s) {
         
         // Approach 1:- using stacks
         // Here we don't want which to delete we just have to increase the count
-        return a1(s);
+        // T.C :- O(n) , S.C :- O(n)
+
+        // return a1(s);
+
+        // Approach 2:- using simply simulating the array
+
+        return a2(s);
     }
 };
