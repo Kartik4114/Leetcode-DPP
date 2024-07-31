4801537aa6fbc4d5250a1bf5742b37c1cbf08ef7
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-01 00:34:00 +0530

    Time: 208 ms (24.77%), Space: 113.1 MB (20.17%) - LeetHub

--- 0869-similar-string-groups/0869-similar-string-groups.cpp
+++ 0869-similar-string-groups/0869-similar-string-groups.cpp
@@ -23,7 +23,6 @@ public:
         unordered_map<int,vector<int>> adj;
 
         int m=strs.size();
-        int n=strs[0].size();
         for(int i=0;i<m;i++){
 
             for(int j=i+1;j<m;j++){
@@ -46,10 +45,64 @@ public:
         }
         return cnt;
     }
+
+    vector<int> parent;
+    vector<int> rank;
+    int find(int x){
+        if(x==parent[x]) return x;
+        return parent[x]=find(parent[x]);
+    }
+
+    void Union(int x,int y){
+
+        int parent_x=find(x);
+        int parent_y=find(y);
+
+        if(parent_x==parent_y) return;
+
+        if(rank[parent_x]<rank[parent_y]){
+            parent[parent_x]=parent_y;
+        } else if(rank[parent_y]<rank[parent_x]){
+            parent[parent_y]=parent_x;
+        } else {
+            parent[parent_y]=parent_x;
+            rank[parent_x]++;
+        }
+    }
+
+    int a2(vector<string> &strs){
+        
+        
+        int n=strs.size();
+        parent.resize(n);
+        rank.resize(n,0);
+
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+        int groupCount=n;
+
+        unordered_map<int,vector<int>> adj;
+
+        for(int i=0;i<n;i++){
+
+            for(int j=i+1;j<n;j++){
+
+                if(isSimilar(strs[i],strs[j]) && find(i)!=find(j)){
+                    Union(i,j);
+                    groupCount--;
+                }
+            }
+        }
+        return groupCount;
+    }
     int numSimilarGroups(vector<string>& strs) {
         
         // APPROACH 1:- USING DFS AND BFS
         // SIMILARLY WE CAN CHECK FOR BFS
-        return a1(strs);
+        // return a1(strs);
+
+        // APPROACH 2:- USING DSU
+        return a2(strs);
     }
 };
