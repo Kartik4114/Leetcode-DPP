f8fcb94d460cf7bbcbf641325339878eaba83f60
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-02 00:05:51 +0530

    Time: 2222 ms (13.88%), Space: 402.6 MB (9.41%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
@@ -1,57 +1,31 @@
-#include <vector>
-#include <unordered_map>
-
 class Solution {
 public:
-    bool dfs(unordered_map<int,vector<int>>& adj,vector<bool>& visited, int s, int d) {
+    bool dfs(unordered_map<int,vector<int>> &adj,vector<bool> &visited,int s,int d){
+        visited[s]=true;
+
         if(s==d) return true;
 
-        if(visited[s]) return false;
-        visited[s] = true;
-
-        for (auto &v : adj[s]) {
-            if (dfs(adj, visited, v, d)==true) {
-                return true;
-            }
-        }
-        return false;
-
-    }
-    bool bfs(unordered_map<int,vector<int>>& adj,vector<bool>& visited, int s, int d){
-        visited[s]=true;
-        queue<int> que;
-        que.push(s);
-
-        while(!que.empty()){
-            
-            int front=que.front();
-            que.pop();
-            if(front==d) return true;
-            for(auto &v:adj[front]){
-
-                if(!visited[v]){
-                    visited[v]=true;
-                    que.push(v);
-                }
+        for(auto &v:adj[s]){
+            if(!visited[v]){
+                if(dfs(adj,visited,v,d)) return true;
             }
         }
         return false;
     }
-
-    bool validPath(int n,vector<vector<int>>& edges, int source, int destination) {
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        
         unordered_map<int,vector<int>> adj;
 
-        for (const auto& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
+        for(auto &edge:edges){
+            int u=edge[0];
+            int v=edge[1];
 
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
 
-        vector<bool> visited(n, false);
-
-        // return dfs(adj, visited, source, destination);
-        return bfs(adj, visited, source, destination);
+        vector<bool> visited(n,false);
+        bool ans=dfs(adj,visited,source,destination);
+        return ans;
     }
 };
