f227175dcab82a89db3f902abc8c67907ec5fbea
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-02 01:19:58 +0530

    Time: 180 ms (92.43%), Space: 97.4 MB (94.39%) - LeetHub

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/1661-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/1661-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        
-        // unordered_map<int,vector<int>> mp;
-        vector<int> inDegree(n,0);
+
+        vector<bool> inDegree(n,false);
 
         for(auto &edge:edges){
             int u=edge[0];
             int v=edge[1];
 
-            inDegree[v]++;
+            inDegree[v]=true;
         }
 
         vector<int> result;
         for(int i=0;i<n;i++){
-            if(inDegree[i]==0) result.push_back(i);
+            if(inDegree[i]==false) result.push_back(i);
         }
         return result;
     }
