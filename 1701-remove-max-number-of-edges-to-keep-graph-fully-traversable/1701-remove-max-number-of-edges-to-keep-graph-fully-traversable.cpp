43648f4e6ebd463cf56c08e301ea72b88f20eaff
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-02 01:09:01 +0530

    Time: 429 ms (20.75%), Space: 142.7 MB (48.98%) - LeetHub

--- 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/1701-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/1701-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,71 +1,94 @@
+class DSU{
+    public:
+        vector<int> parent;
+        vector<int> rank;
+        int components;
+
+        DSU(int n){
+            parent.resize(n+1);
+            for(int i=0;i<n+1;i++){
+                parent[i]=i;
+            }
+            rank.resize(n+1,0);
+            components=n;
+        }
+        int find(int x){
+            if(x==parent[x]) return x;
+
+            return parent[x]=find(parent[x]);
+        }
+        void Union(int x,int y){
+            
+            int parent_x=find(x);
+            int parent_y=find(y);
+
+            if(parent_x==parent_y) return;
+
+            if(rank[parent_x]>rank[parent_y]){
+                parent[parent_y]=parent_x;
+            } else if(rank[parent_y]>rank[parent_x]){
+                parent[parent_x]=parent_y;
+            } else {
+                parent[parent_y]=parent_x;
+                rank[parent_x]++;
+            }
+            components--;
+        }
+
+        bool isSingleComponent(){
+            return components==1;
+        }
+};
 class Solution {
 public:
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        class UnionFind {
-        public:
-            vector<int> parent, size;
-            int components;
-            UnionFind(int n) {
-                components = n;
-                parent.resize(n + 1);
-                size.resize(n + 1, 1);
-                for (int i = 0; i <= n; ++i) {
-                    parent[i] = i;
-                }
-            }
-
-            int find(int x) {
-                if (parent[x] != x) {
-                    parent[x] = find(parent[x]);
-                }
-                return parent[x];
-            }
-
-            bool unite(int x, int y) {
-                int rootX = find(x), rootY = find(y);
-                if (rootX == rootY) return false;
-                if (size[rootX] < size[rootY]) swap(rootX, rootY);
-                parent[rootY] = rootX;
-                size[rootX] += size[rootY];
-                components--;
-                return true;
-            }
-
-            bool isConnected() {
-                return components == 1;
-            }
-        };
         
-        UnionFind alice(n), bob(n);
-        int edgesRequired = 0;
-        
-        // Process type 3 edges first
-        for (const auto& edge : edges) {
-            if (edge[0] == 3) {
-                if (alice.unite(edge[1], edge[2]) | bob.unite(edge[1], edge[2])) {
-                    edgesRequired++;
+        DSU Alice(n);
+        DSU Bob(n);
+
+        // auto lambda=[&](vector<int> &v1,vector<int> &v2){
+        //     return v1[0]>v2[0];
+        // };
+        sort(edges.begin(),edges.end(),greater<vector<int>>());
+
+        int edgeCount=0;
+        for(auto &edge:edges){
+
+            int type=edge[0];
+            int u=edge[1];
+            int v=edge[2];
+            
+            if(type==3){
+                bool edgeAdded=false;
+                if(Alice.find(u)!=Alice.find(v)){
+                    Alice.Union(u,v);
+                    edgeAdded=true;
+                }
+
+                if(Bob.find(u)!=Bob.find(v)){
+                    Bob.Union(u,v);
+                    edgeAdded=true;
+                }
+                if(edgeAdded){
+                    edgeCount++;
+                }
+            } else if(type==2){
+                if(Bob.find(u)!=Bob.find(v)){
+                    Bob.Union(u,v);
+                    edgeCount++;
+                }
+            } else{
+                if(Alice.find(u)!=Alice.find(v)){
+                    Alice.Union(u,v);
+                    edgeCount++;
                 }
             }
         }
-        
-        // Process type 1 and type 2 edges
-        for (const auto& edge : edges) {
-            if (edge[0] == 1) {
-                if (alice.unite(edge[1], edge[2])) {
-                    edgesRequired++;
-                }
-            } else if (edge[0] == 2) {
-                if (bob.unite(edge[1], edge[2])) {
-                    edgesRequired++;
-                }
-            }
+
+        // cout<<edgeCount<<endl;
+        if(Alice.isSingleComponent() && Bob.isSingleComponent()){
+            return edges.size()-edgeCount;
         }
-        
-        // Check if both are fully connected
-        if (alice.isConnected() && bob.isConnected()) {
-            return edges.size() - edgesRequired;
-        }
-        
         return -1;
     }
 };
