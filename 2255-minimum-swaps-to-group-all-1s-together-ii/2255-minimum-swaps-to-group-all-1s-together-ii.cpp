7ed63e6f15c791b431fe9dc20cebd78870c14aba
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-03 00:40:40 +0530

    Time: 78 ms (21.61%), Space: 90.1 MB (21.22%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -9,29 +9,29 @@ public:
             temp[i]=nums[i%n];
         }
 
-        int cntOne=0;
+        int totalOnes=0;
         for(auto num:nums){
-            if(num==1) cntOne++;
+            totalOnes+=num;
         }
-
-        int windowSize=cntOne;
 
         int i=0;
         int j=0;
-        int maxCurrOneCount=0;
-        int currCountOne=0;
-        while(i<2*n){
 
-            while(j<2*n && j-i+1<=windowSize){
-                if(temp[j]==1) currCountOne++;
-                j++;
+        int currOnes=0;
+        int maxCount=0;
+
+        while(j<2*n){
+            if(temp[j]==1) currOnes++;
+
+            if(j-i+1>totalOnes){
+                currOnes-=temp[i];
+                i++;
             }
-            maxCurrOneCount=max(maxCurrOneCount,currCountOne);
-            if(temp[i]==1) currCountOne--;
-            i++;
-        }
 
-        return cntOne-maxCurrOneCount;
+            maxCount=max(maxCount,currOnes);
+            j++;
+        }
+        return totalOnes-maxCount;
 
     }
     int minSwaps(vector<int>& nums) {
