553e36e2bd0b0cc79880430ee8cb2f91acf57152
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-03 19:34:32 +0530

    Time: 20 ms (5.23%), Space: 19.7 MB (16.35%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        
+    bool a1(vector<int> &target,vector<int> &arr){
+
         sort(target.begin(),target.end());
         sort(arr.begin(),arr.end());
 
@@ -10,5 +10,34 @@ public:
             if(arr[i]!=target[i]) return false;
         }
         return true;
+    }
+
+    bool a2(vector<int> &target,vector<int> &arr){
+        
+        unordered_map<int,int> mp;
+        for(auto &t:target){
+            mp[t]++;
+        }
+
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(mp[arr[i]]==0){
+                cout<<"Enter"<<" ";
+                return false;
+            }
+            cout<<arr[i]<<" "<<mp[arr[i]]<<endl;
+            mp[arr[i]]--;
+        }
+        return true;
+    }
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        
+        // APPROACH 1:- using sorting 
+        // T.C :- O(nlogn) , S.C :- O(1)
+        // return a1(target,arr);
+
+        // APPROACH 2:- using map
+        // T.C :- O(n) , S.C :- O(n)
+        return a2(target,arr);
     }
 };
