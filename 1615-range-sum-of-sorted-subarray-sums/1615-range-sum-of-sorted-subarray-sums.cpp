a1b6ecefe5c8a9f1482e72c7d64b47801e148c63
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-04 23:17:27 +0530

    Time: 79 ms (79.33%), Space: 11.2 MB (84.72%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int mod=1e9+7;
-    int rangeSum(vector<int>& nums, int n, int left, int right) {
-        
-        // int size=(n*(n+1))/2;
-
+    int a1(vector<int>& nums, int n, int left, int right){
         vector<int> temp;
 
         for(int i=0;i<n;i++){
@@ -23,5 +20,45 @@ public:
             result=(result+temp[i])%mod;
         }
         return result;
+    }
+
+    typedef pair<int,int> P;
+    int a2(vector<int>& nums, int n, int left, int right){
+        
+        priority_queue<P,vector<P>,greater<P>> pq;
+
+        for(int i=0;i<n;i++){
+            pq.push({nums[i],i});
+        }
+
+        int result=0;
+        for(int count=1;count<=right;count++){
+            auto p=pq.top();
+            pq.pop();
+
+            int sum=p.first;
+            int idx=p.second;
+
+            if(count>=left){
+                result=(result+sum)%mod;
+            }
+
+            int new_idx=idx+1;
+            P newPair;
+            if(new_idx<n){
+                newPair.first=sum+nums[new_idx];
+                newPair.second=new_idx;
+                pq.push(newPair);
+            }
+        }
+        return result;
+    }
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+
+        // approach 1:- O(n^2*logn)
+        // return a1(nums,n,left,right);
+
+        // Approach 2:- O(n^2*logn);
+        return a2(nums,n,left,right);
     }
 };
