f55f544aa018d9990390e47ae1a4600c6f710551
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-04 16:09:45 +0530

    Time: 317 ms (80%), Space: 193.7 MB (60%) - LeetHub

---
+++ 3524-minimum-number-of-flips-to-make-binary-grid-palindromic-ii/3524-minimum-number-of-flips-to-make-binary-grid-palindromic-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int minFlips(vector<vector<int>>& grid) {
+        
+        // contest biweekly 136
+        int m=grid.size();
+        int n=grid[0].size();
+
+        int minChanges=0;
+        for(int i=0;i<m/2;i++){
+            for(int j=0;j<n/2;j++){
+
+                int x=grid[i][j];
+                x+=grid[i][n-j-1];
+                x+=grid[m-i-1][j];
+                x+=grid[m-i-1][n-j-1];
+
+                minChanges+=min(x,4-x);
+            }
+        }
+
+
+        int midOne=0; // when row is odd
+        int pairOne=0;
+        if(m%2==1){
+            for(int j=0;j<n/2;j++){
+                if(grid[m/2][j]!=grid[m/2][n-j-1]){
+                    minChanges++;
+                    midOne++;
+                }
+                if(grid[m/2][j]==grid[m/2][n-j-1] && grid[m/2][j]==1){
+                    pairOne++;
+                }
+            }
+        }
+
+        if(n%2==1){
+            for(int j=0;j<m/2;j++){
+                if(grid[j][n/2]!=grid[m-j-1][n/2]){
+                    minChanges++;
+                    midOne++;
+                }
+                if(grid[j][n/2]==grid[m-j-1][n/2] && grid[j][n/2]==1){
+                    pairOne++;
+                }
+            }
+        }
+
+        if(pairOne%2==1 && midOne==0) minChanges+=2;
+
+        if(n%2==1 && m%2==1 && (grid[m/2][n/2]==1)) minChanges++;
+        return minChanges;
+
+
+
+    }
+};
