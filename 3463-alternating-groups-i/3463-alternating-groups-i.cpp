507adc933c6399edafa54dde8759f3b93ecb3e2c
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-04 23:37:46 +0530

    Time: 3 ms (87.38%), Space: 26.6 MB (8.74%) - LeetHub

--- 3463-alternating-groups-i/3463-alternating-groups-i.cpp
+++ 3463-alternating-groups-i/3463-alternating-groups-i.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int numberOfAlternatingGroups(vector<int>& colors) {
+    int a1(vector<int> &colors){
         int n = colors.size();
         vector<int> temp(n + 2);
 
@@ -17,5 +17,28 @@ public:
             }
         }
         return cnt;
+    }
+
+    int a2(vector<int> &colors){
+        int n = colors.size();
+
+        int cnt = 0;
+        // Correct loop range to check within the bounds of temp
+        for (int i = 0; i < n; i++) {
+            if (colors[i%n] != colors[(i + 1)%n] && colors[(i + 1)%n] != colors[(i + 2)%n]) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    int numberOfAlternatingGroups(vector<int>& colors) {
+        
+        // approach 1:-
+        // T.C :- O(n), S.C :O(n) || O(1)
+        // return a1(colors);
+
+        // approach 2:-
+        // T.C :- O(n), S.C :O(1)
+        return a2(colors);
     }
 };
