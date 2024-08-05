4e76081fd26d640e3b4f31c25f95e8989f1cd19c
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-05 21:43:31 +0530

    Time: 9 ms (29.75%), Space: 15.5 MB (76.27%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        
+        auto lambda=[](int &a,int &b){
+
+            string s1=to_string(a);
+            string s2=to_string(b);
+
+            if(s1+s2>s2+s1) return true;
+            return false;
+        };
+
+        sort(nums.begin(),nums.end(),lambda);
+
+        // Corner case
+        if(nums[0]==0) return "0";
+        string result="";
+
+        for(auto &num:nums){
+            result+=to_string(num);
+        }
+        return result;
+    }
+};
