026a250edb1c892ce52a7b345ab3ec9f90c1c349
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-13 15:36:46 +0530

    Time: 750 ms (49.47%), Space: 54.3 MB (79.78%) - LeetHub

--- 3535-find-the-count-of-monotonic-pairs-i/3535-find-the-count-of-monotonic-pairs-i.cpp
+++ 3535-find-the-count-of-monotonic-pairs-i/3535-find-the-count-of-monotonic-pairs-i.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-    int memo[2001][51][51];
     int mod=1e9+7;
+    int memo[2001][51][51];
     int solve(vector<int> &nums,int idx,int prevI,int prevD){
         int n=nums.size();
         if(idx>=n) return 1;
 
         if(memo[idx][prevI][prevD]!=-1) return memo[idx][prevI][prevD];
+        
         int ans=0;
-
         for(int i=prevI;i<=nums[idx];i++){
-            
+
             int x1=i;
             int x2=nums[idx]-i;
+
             if(x1>=prevI && x2<=prevD){
                 ans=(ans+solve(nums,idx+1,x1,x2))%mod;
             }
         }
-        return memo[idx][prevI][prevD]=ans%mod;
+        
+        return memo[idx][prevI][prevD]=ans;
+    }   
+    int a1(vector<int> &nums){
+
+        int n=nums.size();
+        memset(memo,-1,sizeof(memo));
+        return solve(nums,0,0,nums[0]);
 
     }
     int countOfPairs(vector<int>& nums) {
         
-        memset(memo,-1,sizeof(memo));
-        return solve(nums,0,0,50);
+        // APPROACH 1:- USING RECURSION + MEMO
+        return a1(nums);
     }
 };
