9662076c5c203412ae7d18d8cc27d25d2dfa0e52
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-13 10:18:47 +0530

    Time: 552 ms (55.88%), Space: 54.2 MB (82.44%) - LeetHub

--- 3535-find-the-count-of-monotonic-pairs-i/3535-find-the-count-of-monotonic-pairs-i.cpp
+++ 3535-find-the-count-of-monotonic-pairs-i/3535-find-the-count-of-monotonic-pairs-i.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
-    int M = 1e9+7;
-    int countOfPairs(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(55,0));
+    int memo[2001][51][51];
+    int mod=1e9+7;
+    int solve(vector<int> &nums,int idx,int prevI,int prevD){
+        int n=nums.size();
+        if(idx>=n) return 1;
 
-        // dp[i][j] = number of pairs till ith index when last number is j
-        // ans = sum of dp[n-1][j] where j is from 0 to 50
+        if(memo[idx][prevI][prevD]!=-1) return memo[idx][prevI][prevD];
+        int ans=0;
 
-        // dp[0][j] = selecting till 0th index, taking last number as 0 to nums[0]
-        for(int curr_num = 0; curr_num <= nums[0]; curr_num++)
-            dp[0][curr_num] = 1;
-
-        for(int ind=1; ind<n; ind++) // n
-        {
-            for(int curr_num = 0; curr_num <= nums[ind]; curr_num++) // 50
-            {
-                int ways = 0;
-                for(int prev_num = 0; prev_num <= 50; prev_num++) // 50
-                {
-                    if(prev_num <= curr_num && (nums[ind] - curr_num <= nums[ind-1] - prev_num))
-                    {
-                        ways = ( ways%M + dp[ind-1][prev_num]%M ) %M;
-                    }
-                }
-                dp[ind][curr_num] = ways;
+        for(int i=prevI;i<=nums[idx];i++){
+            
+            int x1=i;
+            int x2=nums[idx]-i;
+            if(x1>=prevI && x2<=prevD){
+                ans=(ans+solve(nums,idx+1,x1,x2))%mod;
             }
-        } // n * 50 * 50
-        
-        int total_count_of_pairs = 0;
-        for(int curr_num = 0; curr_num <= 50; curr_num++)
-            total_count_of_pairs = ( total_count_of_pairs%M + dp[n-1][curr_num]%M ) %M;
+        }
+        return memo[idx][prevI][prevD]=ans%mod;
 
-        return total_count_of_pairs;
     }
-    // tc - 250*2000 = 500000 = O(5*10^5)
-    // sc - 2000*50 = 100000 = O(10^5)
+    int countOfPairs(vector<int>& nums) {
+        
+        memset(memo,-1,sizeof(memo));
+        return solve(nums,0,0,50);
+    }
 };
