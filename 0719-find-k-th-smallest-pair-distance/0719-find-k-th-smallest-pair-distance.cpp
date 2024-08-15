b87484bf62c376d0b753f8185f4c814abc550b56
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-15 06:11:02 +0530

    Time: 625 ms (6.55%), Space: 26 MB (5.07%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    int smallestDistancePair(std::vector<int>& numbers, int k) {
-        std::sort(numbers.begin(), numbers.end());
-        int minDistance = 0, maxDistance = numbers.back() - numbers.front();
+    int smallestDistancePair(vector<int>& nums, int k) {
         
-        while (minDistance < maxDistance) {
-            int midDistance = minDistance + (maxDistance - minDistance) / 2;
-            if (countPairsWithinDistance(numbers, midDistance) < k) {
-                minDistance = midDistance + 1;
-            } else {
-                maxDistance = midDistance;
-            }
-        }
-        
-        return minDistance;
-    }
+        int n=nums.size();
+        int maxElement=*max_element(nums.begin(),nums.end());
 
-private:
-    int countPairsWithinDistance(const std::vector<int>& numbers, int targetDistance) {
-        int count = 0, left = 0;
-        for (int right = 1; right < numbers.size(); ++right) {
-            while (numbers[right] - numbers[left] > targetDistance) {
-                ++left;
+        vector<int> vec(maxElement+1,0);
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+
+                int diff=abs(nums[j]-nums[i]);
+                vec[diff]++;
             }
-            count += right - left;
         }
-        return count;
+
+        for(int i=0;i<vec.size();i++){
+
+            k-=vec[i];
+            if(k<=0) return i;
+        }
+        return -1;
     }
 };
