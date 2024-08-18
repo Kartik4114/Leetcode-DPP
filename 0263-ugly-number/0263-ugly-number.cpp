488a3617f73853dec85df86b620325bcbbb387cb
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-18 14:19:58 +0530

    Time: 5 ms (24.16%), Space: 7.4 MB (41.97%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    bool isUgly(int n) {
-        
+    bool a1(int n){
         if(n<=0) return false;
         long long count=0;
         while(n>1){
@@ -23,11 +22,27 @@ public:
                 count=0;
             }
             else count++;
-
-            // if(count==0) break;
             if(count>=3) return false;
         }
 
         return true;
+    }
+
+    bool a2(int n){
+        if(n<=0) return false;
+
+        while(n%2==0) n=n/2;
+        while(n%3==0) n=n/3;
+        while(n%5==0) n=n/5;
+
+        return n==1;
+    }
+    bool isUgly(int n) {
+        
+        // APPROACH 1:- 
+        // return a1(n);
+
+        // APPROACH 2:- 
+        return a2(n);
     }
 };
