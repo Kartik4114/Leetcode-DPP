b3f09f0339001d089678867147b0d46c985e730e
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-21 11:30:02 +0530

    Time: 17 ms (84.22%), Space: 18.4 MB (65.91%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,38 +1,72 @@
 class Solution {
 public:
-    void BFS(vector<vector<int>> &isConnected,vector<bool> &visited,int u){
+    void DFS(vector<vector<int>> &isConnected,int u,vector<bool> &visited){
         
         int n=isConnected.size();
+        visited[u]=true;
 
+        for(int v=0;v<n;v++){
+            
+            if(!visited[v] && isConnected[u][v]==1){
+                visited[v]=true;
+                DFS(isConnected,v,visited);
+            }
+        }
+    }
+    int a1(vector<vector<int>>& isConnected){
+        int n=isConnected.size();
+        int cnt=0;
+        vector<bool> visited(n,false);
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                cnt++;
+                DFS(isConnected,i,visited);
+                
+            }
+        }
+        return cnt;
+    }
+
+    void BFS(vector<vector<int>>& adj,int u,vector<bool> &visited){
+
+        int n=adj.size();
         queue<int> que;
+
         que.push(u);
         visited[u]=true;
 
         while(!que.empty()){
-
             int front=que.front();
             que.pop();
+
             for(int v=0;v<n;v++){
-                if(!visited[v] && isConnected[front][v]==1){
+                if(!visited[v] && adj[front][v]==1){
                     que.push(v);
                     visited[v]=true;
                 }
             }
         }
-    }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        
-        int cnt=0;
-        int n=isConnected.size();
 
+    }
+    int a2(vector<vector<int>>& isConnected){
+        int n=isConnected.size();
+        int cnt=0;
         vector<bool> visited(n,false);
-        
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                BFS(isConnected,visited,i);
                 cnt++;
+                BFS(isConnected,i,visited);
             }
         }
         return cnt;
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        
+        // APPROACH 1:- USING DFS
+        return a1(isConnected);
+
+        // APPROACH 2:- USING BFS
+        return a2(isConnected);
     }
 };
