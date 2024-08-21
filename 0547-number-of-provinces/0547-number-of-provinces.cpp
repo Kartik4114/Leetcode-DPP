9ca0908ba438241fb46307c78d1797ea73978e21
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-21 11:23:09 +0530

    Time: 17 ms (84.22%), Space: 18.2 MB (86.61%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,72 +1,27 @@
 class Solution {
 public:
-    void DFS(vector<vector<int>> &isConnected,int u,vector<bool> &visited){
-        
+    void DFS(vector<vector<int>> &isConnected,vector<bool> &visited,int u){
         int n=isConnected.size();
         visited[u]=true;
 
         for(int v=0;v<n;v++){
-            
             if(!visited[v] && isConnected[u][v]==1){
-                visited[v]=true;
-                DFS(isConnected,v,visited);
+                DFS(isConnected,visited,v);
             }
         }
     }
-    int a1(vector<vector<int>>& isConnected){
-        int n=isConnected.size();
-        int cnt=0;
-        vector<bool> visited(n,false);
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                cnt++;
-                DFS(isConnected,i,visited);
-                
-            }
-        }
-        return cnt;
-    }
-
-    void BFS(vector<vector<int>>& adj,int u,vector<bool> &visited){
-
-        int n=adj.size();
-        queue<int> que;
-
-        que.push(u);
-        visited[u]=true;
-
-        while(!que.empty()){
-            int front=que.front();
-            que.pop();
-
-            for(int v=0;v<n;v++){
-                if(!visited[v] && adj[front][v]==1){
-                    que.push(v);
-                    visited[v]=true;
-                }
-            }
-        }
-
-    }
-    int a2(vector<vector<int>>& isConnected){
-        int n=isConnected.size();
-        int cnt=0;
-        vector<bool> visited(n,false);
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                cnt++;
-                BFS(isConnected,i,visited);
-            }
-        }
-        return cnt;
-    }
-
     int findCircleNum(vector<vector<int>>& isConnected) {
         
-        // APPROACH 1:- USING DFS
-        // return a1(isConnected);
+        int cnt=0;
+        int n=isConnected.size();
 
-        // APPROACH 2:- USING BFS
-        return a2(isConnected);
+        vector<bool> visited(n,false);
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                DFS(isConnected,visited,i);
+                cnt++;
+            }
+        }
+        return cnt;
     }
 };
