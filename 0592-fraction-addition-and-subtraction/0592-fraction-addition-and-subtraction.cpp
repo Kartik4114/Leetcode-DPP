4c43198bbb4c85e0e751592001f12847e013260b
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-23 21:51:50 +0530

    Time: 3 ms (50.39%), Space: 7.6 MB (74.81%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
@@ -30,8 +30,8 @@ public:
         int ans=stoi(num);
         return ans;
     }
-    string fractionAddition(string expression) {
-        
+
+    string a1(string &expression){
         int n=expression.size();
 
         vector<int> num;
@@ -73,5 +73,64 @@ public:
         result+='/';
         result+=denoStr;
         return result;
+    }
+
+    string a2(string &expr){
+
+        int nume=0;
+        int deno=1;
+
+        int i=0;
+        int n=expr.length();
+        while(i<n){
+
+            int currNume=0;
+            int currDeno=0;
+
+            bool isNeg=(expr[i]=='-');
+
+            if(expr[i]=='+' || expr[i]=='-') i++;
+
+            //Build the currNume
+            while(i<n && isdigit(expr[i])){
+                int val=expr[i]-'0';
+                currNume=(currNume*10) +val;
+                i++;
+            }
+
+            i++; //numerator / denominator //skipiing the divisrio character '/'
+            
+            if(isNeg==true) currNume*=-1;
+
+            //Build the currDeno
+            while(i<n && isdigit(expr[i])){
+                int val=expr[i]-'0';
+                currDeno=(currDeno*10) +val;
+                i++;
+            }
+
+            nume=nume*currDeno + deno*currNume;
+            deno=deno*currDeno;
+        }
+
+        //num
+        //deno
+        //-3 / 6 //GCD : 3
+        int GCD=abs(__gcd(nume,deno));
+
+        nume/=GCD;
+        deno/=GCD;
+
+        return to_string(nume) + '/' + to_string(deno);
+    }
+    string fractionAddition(string expr) {
+        
+        // APPROACH 1:-
+        // T.C :- O(n) , S.C :- O(n)
+        // return a1(expr);
+
+        // APPROACH 2:-
+        // T.C :- O(n) , S.C :- O(1)
+        return a2(expr);
     }
 };
