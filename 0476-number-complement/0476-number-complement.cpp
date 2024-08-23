d64bd66ffdfbea7e8f26738e37a5bf5123e7d1ee
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-23 22:54:59 +0530

    Time: 2 ms (50.2%), Space: 7.6 MB (34.03%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -9,8 +9,27 @@ public:
         }
         return num;
     }
+
+    int a2(int num){
+
+        int numBits=(int)log2(num)+1;
+
+        unsigned int mask=(1U << numBits)-1; // using simple int is giving overflow integer
+
+        return (mask^num);
+    }
     int findComplement(int num) {
-        // APPROACH 1:- USING XOR
-        return a1(num);
+
+        //Approach-1 (Using bit magic of XOR)
+        //T.C : O(log2(num)) // BEC. FOR LOOP IS RUNNING LOGN TIMES
+        //S.C : O(1)
+        // return a1(num);
+
+        //Approach-2 (Using XOR mask)
+        //T.C : O(log2(num))
+        //S.C : O(1)
+        return a2(num);
+
+        // APPROACH 3:- 
     }
 };
