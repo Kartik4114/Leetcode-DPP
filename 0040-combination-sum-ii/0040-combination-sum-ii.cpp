8f684e759a8130fab30dad5aa1d1f9d6af96960f
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-24 17:03:07 +0530

    Time: 6 ms (35.85%), Space: 12.9 MB (41.24%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> result;
-    unordered_map<int,int> mp;
+    void solve(vector<int> &vec,int t,vector<vector<int>> &result,vector<int> &temp,int idx){
+        int n=vec.size();
+        if(t<0) return;
 
-    void helper(vector<int> &c, vector<int> &temp, int target, int sum, int idx){
-        int n = c.size();
-        if (sum > target) return;
-
-        if (sum == target) {
+        if(t==0){
             result.push_back(temp);
             return;
         }
 
-        for (int i = idx; i < n; ++i) {
-            if (i > idx && c[i] == c[i - 1]) continue; // Skip duplicates
+        for(int i=idx;i<n;i++){
+            if(i>idx && vec[i]==vec[i-1]) continue;
 
-            if (mp[c[i]] > 0) {
-                sum += c[i];
-                mp[c[i]]--;
-                temp.push_back(c[i]);
-
-                helper(c, temp, target, sum, i); // not i+1 because we can reuse the same element
-
-                sum -= c[i];
-                mp[c[i]]++;
-                temp.pop_back();
-            }
+            temp.push_back(vec[i]);
+            solve(vec,t-vec[i],result,temp,i+1);
+            temp.pop_back();
         }
     }
-
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+
+        // T.C :- O(2^n) , S.C :- O(n) (auxillary space)
+        vector<vector<int>> result;
         vector<int> temp;
-        sort(candidates.begin(), candidates.end()); // Sort candidates to handle duplicates
-        for (auto c : candidates) {
-            mp[c]++;
-        }
-        helper(candidates, temp, target, 0, 0);
+        
+        sort(candidates.begin(),candidates.end());
+        solve(candidates,target,result,temp,0);
         return result;
     }
 };
