ac121a35bd0e7cbee06771b97ed9399485d6c63e
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-08-31 18:56:47 +0530

    Time: 190 ms (67.76%), Space: 79.4 MB (36.76%) - LeetHub

--- 3534-count-almost-equal-pairs-i/3534-count-almost-equal-pairs-i.cpp
+++ 3534-count-almost-equal-pairs-i/3534-count-almost-equal-pairs-i.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     bool isAlmostEqual(int a,int b){
+        
         vector<pair<int,int>> temp;
-
         while(a>0 || b>0){
 
             int rem_a=a%10;
             int rem_b=b%10;
 
-            a=a/10;
-            b=b/10;
+            a/=10;
+            b/=10;
 
             if(rem_a!=rem_b){
-                if(temp.size()>2) return false;
                 temp.push_back({rem_a,rem_b});
             }
-
+            if(temp.size()>2) return false;
+            
         }
-
         if(temp.size()==0) return true;
 
         if(temp.size()==2){
@@ -25,6 +24,7 @@ public:
                 return true;
             }
         }
+        
         return false;
     }
     int countPairs(vector<int>& nums) {
@@ -34,12 +34,13 @@ public:
         int pairCnt=0;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                
+
                 if(isAlmostEqual(nums[i],nums[j])){
                     pairCnt++;
                 }
             }
         }
+
         return pairCnt;
     }
 };
