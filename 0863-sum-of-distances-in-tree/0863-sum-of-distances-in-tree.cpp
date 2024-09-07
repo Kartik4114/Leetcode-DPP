092923ba0a2a65577a69d6364c288116d1e5a3c3
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-09-07 17:01:24 +0530

    Time: 529 ms (10.54%), Space: 113.7 MB (19.05%) - LeetHub

--- 0863-sum-of-distances-in-tree/0863-sum-of-distances-in-tree.cpp
+++ 0863-sum-of-distances-in-tree/0863-sum-of-distances-in-tree.cpp
@@ -1,57 +1,54 @@
 class Solution {
 public:
-    long rootResult;
     vector<int> count;
+    int rootResult;
     int N;
 
     int dfsBase(unordered_map<int,vector<int>> &adj,int currNode,int prevNode,int currDepth){
-        int totalCount=1; // count of node below count
-
+        int totalCount=1;
         rootResult+=currDepth;
-        for(auto &v:adj[currNode]){
 
-            if(v==prevNode) continue;
-
-            totalCount+=dfsBase(adj,v,currNode,currDepth+1);
-
+        for(auto &child:adj[currNode]){
+            
+            if(child==prevNode) continue;
+            totalCount+=dfsBase(adj,child,currNode,currDepth+1);
         }
+
         count[currNode]=totalCount;
         return totalCount;
-    }
 
+    }
     void dfs(unordered_map<int,vector<int>> &adj,int parentNode,int prevNode,vector<int> &result){
 
         for(auto &child:adj[parentNode]){
-
             if(child==prevNode) continue;
 
-            result[child]=result[parentNode] - count[child] + N-count[child];
+            result[child]=result[parentNode]-count[child] +(N-count[child]);
             dfs(adj,child,parentNode,result);
         }
     }
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
         
-        unordered_map<int,vector<int>> adj;
-        N=n;
         count.resize(n,0);
+        rootResult=0;
+        N=n;
 
+        unordered_map<int,vector<int>> adj;
         for(auto &edge:edges){
             int u=edge[0];
             int v=edge[1];
-            
+
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
 
-        rootResult=0;
-        
-       vector<int> result(n,0);
-       count[0]=dfsBase(adj,0,-1,0);
-       
-       result[0]=rootResult;
+        vector<int> result(n,0);
+        dfsBase(adj,0,-1,0);
 
-       dfs(adj,0,-1,result);
-       return result;
-        
+        result[0]=rootResult;
+
+        dfs(adj,0,-1,result);
+        return result;
+
     }
 };
