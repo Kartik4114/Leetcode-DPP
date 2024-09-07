f21c377c80e8c6fcaebe6d1fd0750d3f48cd6157
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-09-07 16:00:36 +0530

    Time: 154 ms (40.23%), Space: 70.9 MB (54.83%) - LeetHub

--- 0922-possible-bipartition/0922-possible-bipartition.cpp
+++ 0922-possible-bipartition/0922-possible-bipartition.cpp
@@ -4,81 +4,37 @@ public:
         color[u]=currColor;
 
         for(auto &v:adj[u]){
+
             if(color[v]==color[u]) return false;
 
             if(color[v]==-1){
-                int colorOfV=1-currColor;
-                if(!dfs(adj,color,v,colorOfV)) return false;
-            }
-        }
-        return true;
+                int colorOfV=1-color[u];
 
-    }
-
-    bool a1(int n,unordered_map<int,vector<int>> &adj,vector<int> &color){
-        // 0 for red
-        // 1 for green
-
-        for(int i=1;i<=n;i++){
-            if(color[i]==-1){
-                if(dfs(adj,color,i,0)==false) return false;
-            }
-        }
-        return true;
-    }
-
-    bool bfs(unordered_map<int,vector<int>> &adj,vector<int> &color,int u){
-
-        queue<int> que;
-        que.push(u);
-        color[u]=0;
-
-        while(!que.empty()){
-            int front=que.front();
-            que.pop();
-
-            for(auto &v:adj[front]){
-
-                if(color[v]==color[front]) return false;
-
-                if(color[v]==-1){
-                    int colorOfV=1-color[front];
-                    que.push(v);
-                    color[v]=colorOfV;
-                }
-            }
-        }
-        return true;
-    }
-    bool a2(int n,unordered_map<int,vector<int>> &adj,vector<int> &color){
-        // 0 for red
-        // 1 for green
-
-        for(int i=1;i<=n;i++){
-            if(color[i]==-1){
-                if(bfs(adj,color,i)==false) return false;
+                if(dfs(adj,color,v,colorOfV)==false) return false;
             }
         }
         return true;
     }
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         
+        vector<int> color(n+1,-1);
         unordered_map<int,vector<int>> adj;
 
-        for(int i=0;i<dislikes.size();i++){
-            int per1=dislikes[i][0];
-            int per2=dislikes[i][1];
+        for(auto &dislike:dislikes){
 
-            adj[per1].push_back(per2);
-            adj[per2].push_back(per1);
+            int u=dislike[0];
+            int v=dislike[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-        vector<int> color(n+1,-1);
 
-        // APPROACH 1:- USING DFS
-        // return a1(n,adj,color);
+        for(int i=1;i<n+1;i++){
 
-        // APPROACH 2:- USING BFS
-        return a2(n,adj,color);
-
+            if(color[i]==-1){
+                if(dfs(adj,color,i,0)==false) return false;
+            }
+        }
+        return true;
     }
 };
