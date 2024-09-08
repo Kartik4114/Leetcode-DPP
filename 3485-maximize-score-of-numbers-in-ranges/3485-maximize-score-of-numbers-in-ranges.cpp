afe61c501bf57f4470848ba7a58f6824e4e52a77
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-09-08 22:51:56 +0530

    Time: 233 ms (100%), Space: 224.4 MB (100%) - LeetHub

--- 3485-maximize-score-of-numbers-in-ranges/3485-maximize-score-of-numbers-in-ranges.cpp
+++ 3485-maximize-score-of-numbers-in-ranges/3485-maximize-score-of-numbers-in-ranges.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    
-    bool canAchieve(vector<int> &start,int d,long long midNum){
-        
+    bool canAchieve(vector<int> start,long long mid,int d){ // here not using &operator in start
         int n=start.size();
-        
-        long long numTaken=-1;
-        for(int i=0;i<n;i++){
-            
-            if(i==0){
-                numTaken=start[0];
-                continue;
+
+        for(int i=1;i<n;i++){
+
+            if(start[i]-start[i-1]>=mid) continue;
+
+            else {
+                if(start[i]+d-start[i-1]<mid) return false;
+                start[i]=start[i-1]+mid;
             }
-            
-            long long currNum = max(numTaken + (long long)midNum, (long long)start[i]);
-            if (currNum <=(long long)(start[i] + d)) { 
-                numTaken = currNum; 
-            } else return false;
-             
+
         }
         return true;
     }
     int maxPossibleScore(vector<int>& start, int d) {
         
+        // 
         int n=start.size();
-        
         sort(start.begin(),start.end());
-        
-        long long st=0;
-        long long end=start[n-1]-start[0] +d;
-        
-        int maxScore=0;
-        while(st<=end){
-            
-            long long mid=(st+end)/2;
-            if(canAchieve(start,d,mid)){
+
+        long long low=0;
+        long long high=start[n-1]+d-start[0];
+
+        long long maxScore=0;
+        while(low<=high){
+            long long mid=(low+high)/2;
+
+            if(canAchieve(start,mid,d)){
                 maxScore=mid;
-                st=mid+1;
-            } else end=mid-1;
+                low=mid+1;
+            } else high=mid-1;
         }
-        
+
         return maxScore;
     }
 };
