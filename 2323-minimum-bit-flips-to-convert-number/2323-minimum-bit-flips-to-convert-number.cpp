8958522c0c6d15c0185b65c6560d2baf628ee4d7
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-09-14 19:03:35 +0530

    Time: 0 ms (100%), Space: 7.7 MB (41.78%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int xorResult=start^goal;
+        int xorResult=start ^ goal;
 
         int ans=__builtin_popcount(xorResult);
         return ans;
