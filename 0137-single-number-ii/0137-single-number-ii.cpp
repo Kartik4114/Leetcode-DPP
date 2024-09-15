5ce7e4e02ab73781a32c0fa4e58b369a68f2c0a9
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-09-15 13:20:59 +0530

    Time: 7 ms (50.59%), Space: 12 MB (86.53%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,51 +1,17 @@
 class Solution {
 public:
-    int a3(vector<int> &nums){
-        int result=0;
-
-        for(int k=0;k<=31;k++){
-            int countOne=0;
-            for(auto num:nums){
-                if(num &(1<<k)) countOne++;
-            }
-
-            if(countOne%3==1) {
-                result =(result | (1<<k));
-            }
-        }
-        return result;
-    }
-
-    int a4(vector<int> &nums){
+    int singleNumber(vector<int>& nums) {
+        
         int ones=0;
         int twos=0;
 
-        for(auto num:nums){
-            ones=(ones^num) & ~twos;
-            twos=(twos^num) & ~ones;
+        for(auto &num:nums){
+
+            ones=ones^num & ~twos;
+            twos=twos^num & ~ones;
+
+            cout<<ones<<" "<<twos<<endl;
         }
-
         return ones;
-    }
-    int singleNumber(vector<int>& nums) {
-
-        // Approach 1:- using unordered map
-        // Approach 2:- using sorting
-        
-        //  Time complexity :- O(nlogn) 
-        // Although bit manipulation has O(32*n) but sorting is the best
-        // solution bec. n= 2^32 then nlogn will be 2^32 which is not possible bec.
-        //  n= 2^31-1 
-        
-        // Approach 3 :- BIT MANIPULATION
-//        T.C:- O(32*n) , S.C:- O(1)
-        
-        // return a3(nums);
-
-        // APPROACH 3:- BEST APPROACH USING BUT MANIPULATION
-        // USING CONCEPT OF BUCKETS
-        // T.C:- O(N) , S.C. :- O(1);
-
-            return a4(nums);
     }
 };
