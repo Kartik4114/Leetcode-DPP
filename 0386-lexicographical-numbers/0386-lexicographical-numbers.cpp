d2cf22e957e4feb98b4232d2b0d1dde4fd0caf85
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-09-24 19:50:00 +0530

    Time: 465 ms (5.03%), Space: 15.4 MB (22.71%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -13,13 +13,43 @@ public:
             }
         }
     }
-    vector<int> lexicalOrder(int n) {
-        
+
+    vector<int> a2(int n){
+
         vector<int> result;
         for(int i=1;i<=9;i++){
             solve(n,i,0,result);
         }
 
         return result;
+    }
+
+    vector<int> a1(int n){
+
+        vector<int> result(n);
+        for(int i=0;i<n;i++){
+            result[i]=i+1;
+        }
+
+        auto lambda=[](int x,int y){
+
+            string s1=to_string(x);
+            string s2=to_string(y);
+
+            return s1 < s2;
+        };
+        sort(result.begin(),result.end(),lambda);
+        return result;
+    }
+    vector<int> lexicalOrder(int n) {
+        
+        // APPROACH 1:- USING SORTING 
+        // T.C :- O(nlogn)
+        return a1(n);
+
+        // APPROACH 2:- USING RECURSION 
+        // T.C :- O(N) AS we are visiting every element only once
+        // return a2(n);
+
     }
 };
