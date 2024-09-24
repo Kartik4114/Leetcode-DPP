c9f30354bc42653ba6f7be31393fb0c5f0a1dbd5
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-09-24 20:28:59 +0530

    Time: 0 ms (100%), Space: 7.5 MB (56.96%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -1,27 +1,103 @@
 class Solution {
-private:
-    int getReqNum(long a,long b,long &n){
-        int gap=0; 
-        while(a <= n){
-            gap += min(n+1,b)-a;
-            a*=10;
-            b*=10;
-        }
-        return gap;
-    }
 public:
-    int findKthNumber(long n, int k) {
-        long num = 1;
-        for(int i=1; i<k;){
-            int req = getReqNum(num,num+1,n);
-            if(i+req <= k){
-                i+=req;
-                num++;
-            }else{
-                i++;
-                num *= 10;
+    void solve(int n,int lastNum,vector<int> &result){
+        if(lastNum>n) return;
+        
+        result.push_back(lastNum);
+        for(int i=0;i<=9;i++){
+
+            int nextNum=lastNum*10+i;
+            if(nextNum<=n){
+                solve(n,nextNum,result);
             }
         }
-        return num;
+    }
+
+    int a1(int n,int k){
+        vector<int> result;
+
+        for(int i=1;i<=9;i++){
+            solve(n,i,result);
+        }
+
+        return result[k-1];
+    }
+
+    bool solve(int n,int k,long lastNum,int &count,int &result){
+        if(lastNum>n) return false;
+
+        count++;
+        if(count==k){
+            result=lastNum;
+            return true;
+        }
+
+        for(int i=0;i<=9;i++){
+            long nextNum=lastNum*10+i;
+            if(nextNum>n) break;
+
+            if(solve(n,k,nextNum,count,result)) return true;
+        }
+        return false;
+    }
+    int a2(int n,int k){
+        
+        int result=0;
+        int count=0;
+        for(int i=1;i<=9;i++){
+            if(solve(n,k,i,count,result)) break;
+        }
+        return result;
+        
+    }
+
+    int count(long curr,long next,int n){
+
+        int countNum=0;
+        while(curr<=n){
+
+            countNum+=(next-curr);
+
+            curr*=10;
+            next*=10;
+            next=min(next,long(n+1));
+        }
+        return countNum;
+    }
+    int a3(int n,int k){
+
+        int curr=1;
+        k-=1; //Since we start from the first number (1), we need k-1 more numbers
+
+        while(k>0){
+            int countNum=count(curr,curr+1,n);
+
+            if(countNum<=k){
+                curr++;
+                k-=countNum;
+            } else {
+                curr*=10;
+                k--;
+            }
+        }
+        return curr;
+    }
+    int findKthNumber(int n, int k) {
+        
+        // APPROACH 1:- TLE + MLE
+        // T.C :- O(n) , S.C :- O(n+d) d=system stack space
+        // (BEC. unable to compute 10 ^9 value and store 10^9 values in result vector)
+        // return a1(n,k);
+
+        // APPROACH 2:- TLE
+        // T.C :- O(n) , S.C :- O(d) d=system stack space
+        // (BEC. unable to compute 10 ^9 value)
+        // but for resolving MLE we are using count vector
+        // return a2(n,k);
+
+        // APPROACH 3:- MOST OPTIMISED APPROACH
+        // T.C :- O((logn)*(logn))
+        // S.C :- O(logn)
+        return a3(n,k);
     }
 };
