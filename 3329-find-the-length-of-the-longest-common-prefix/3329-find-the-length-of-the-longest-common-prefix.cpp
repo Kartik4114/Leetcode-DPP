688555c24dad606dbc4b9e1ba89139603a3fdc63
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-09-25 17:53:27 +0530

    Time: 243 ms (76.55%), Space: 160.6 MB (59.4%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        
-        unordered_set<string> st;
+    int a1(vector<int> &arr1,vector<int> &arr2){
+         unordered_set<string> st;
 
         for(int i=0;i<arr1.size();i++){
 
@@ -34,5 +33,82 @@ public:
         }
 
         return maxLen;
+    }
+
+    struct trieNode{
+        trieNode *children[10];
+    };
+    
+    trieNode* getNode(){
+        trieNode* node=new trieNode();
+        for(int i=0;i<10;i++){
+            node->children[i]=NULL;
+        }
+        return node;
+    }
+    void insert(int num,trieNode* root){
+
+        trieNode* pCrawler=root;
+
+        string temp=to_string(num);
+
+        int n=temp.size();
+        for(int i=0;i<n;i++){
+
+            int idx=temp[i]-'0';
+
+            if(pCrawler->children[idx]==NULL){
+                pCrawler->children[idx]=getNode();
+            }
+            pCrawler=pCrawler->children[idx];
+        }
+    }
+
+    int search(int num,trieNode* root){
+
+        string temp=to_string(num);
+        int n=temp.size();
+
+        trieNode* pCrawler=root;
+
+        int count=0;
+        for(int i=0;i<n;i++){
+
+            int idx=temp[i]-'0';
+            if(pCrawler->children[idx]!=NULL){
+                count++;
+                pCrawler=pCrawler->children[idx];
+            }
+
+            else break;
+        }
+        return count;
+    }
+
+    int a2(vector<int> &arr1,vector<int> &arr2){
+
+        trieNode* root=getNode();
+        
+        for(int i=0;i<arr1.size();i++){
+            insert(arr1[i],root);
+        }
+
+        int result=0;
+        for(int i=0;i<arr2.size();i++){
+
+            int count=search(arr2[i],root);
+            result=max(result,count);
+        }
+        return result;
+    }
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        
+        // approach 1:- using unordered map and storing in the string
+        // T.C :- O((m+n)*d) n=arr1.size(), m=arr2.size() , d:- no.of digits
+        // return a1(arr1,arr2);
+
+        // Approach 2:- Using trie
+        return a2(arr1,arr2);
+
     }
 };
