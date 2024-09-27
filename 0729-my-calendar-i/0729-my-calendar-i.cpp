70524ab20901f1045d422614ac538d183550d355
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-09-27 08:33:44 +0530

    Time: 120 ms (23.95%), Space: 42.6 MB (69.62%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,15 +1,26 @@
 class MyCalendar {
 public:
-    vector<pair<int, int>> ranges;
-    MyCalendar() {}
+    unordered_map<int,int> mp;
+    MyCalendar() {
+        
+    }
+    
     bool book(int start, int end) {
-        for (auto const& pair : ranges) {
-            int r_start = pair.first;
-            int r_end = pair.second;
-            if (start < r_end && end > r_start)
-                return false;
+        
+        for(auto &it:mp){
+            if((start<=it.first && end>it.second) || (start<it.second && end>it.first) ||
+                (start<=it.first && end>it.second) || (start>=it.first && end<=it.second)) return false;
         }
-        ranges.push_back(make_pair(start, end));
+        // cout<<"M"<<endl;
+        mp[start]=end;
+        cout<<mp.size()<<endl;
         return true;
+
     }
 };
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
