10f4b5a3089d6f9d12d8ec8dbcd3b07f581ab9e4
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-10-09 08:54:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (55.34%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int minAddToMakeValid(string s) {
-        
+    int a1(string &s){
+
         stack<char> st;
 
         for(auto &ch:s){
@@ -12,5 +12,30 @@ public:
         }
 
         return st.size();
+    }
+
+    int a2(string &s){
+
+        int openCnt=0;
+        int closeCnt=0;
+
+        for(auto &ch:s){
+
+            if(ch=='(') openCnt++;
+            else {
+                if(openCnt>0) openCnt--;
+                else closeCnt++;
+            }
+        }
+
+        return openCnt+closeCnt;
+    }
+    int minAddToMakeValid(string s) {
+        
+        // approach 1:- using stack (T.C :- O(n) , S.C :- O(n))
+        // return a1(s);
+
+        // Approach 2:-
+        return a2(s);
     }
 };
