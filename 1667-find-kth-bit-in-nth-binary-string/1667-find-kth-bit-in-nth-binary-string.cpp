200da5f1b8ba63dbc9faf9f7e5f839877f3b3631
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-10-19 08:00:21 +0530

    Time: 55 ms (23.21%), Space: 48.7 MB (46.42%) - LeetHub

---
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void makeString(int n, string &temp) {
+
+        if (n <= 1) {
+            temp += '0';
+            return;
+        }
+
+        makeString(n - 1, temp);
+
+        string rev = temp;
+        temp += '1';
+
+        for(int i=0;i<rev.size();i++){
+            if(rev[i]=='0') rev[i]='1';
+            else rev[i]='0';
+        }
+        reverse(rev.begin(), rev.end()); // Reverse the copy (rev) instead of original temp
+        temp += rev;
+    }
+
+    char findKthBit(int n, int k) {
+
+        string temp = "";
+        makeString(n, temp);
+
+        cout << temp << endl;
+        return temp[k - 1]; // Return the kth bit (0-based index, so use k-1)
+    }
+};
