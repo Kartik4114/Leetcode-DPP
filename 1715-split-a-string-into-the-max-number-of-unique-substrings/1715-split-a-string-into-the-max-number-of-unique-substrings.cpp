dbae2638069e9c93e2478eac92dc6ff5eab2f0b8
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-10-21 18:07:29 +0530

    Time: 18 ms (97.37%), Space: 12.9 MB (93.27%) - LeetHub

---
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void solve(string &s,int i,unordered_set<string> &st,int &maxCount,int currCount){
+        int n=s.size();
+
+        if(i>=n) { // Base Case
+            maxCount =max(maxCount,currCount);
+            return;
+        }
+
+        // Below condition is not necessay needed but it gives slight improvement
+        if(currCount + n-i <= maxCount) return;
+
+        for(int j=i;j<n;j++){
+
+            string sub=s.substr(i,j-i+1);
+            if(st.find(sub)==st.end()){
+                st.insert(sub);
+                solve(s,j+1,st,maxCount,currCount+1);
+                st.erase(sub);
+            }
+        }
+
+
+    }
+    int maxUniqueSplit(string s) {
+        
+        unordered_set<string> st;
+        int maxCount=0;
+        int currCount=0;
+        int i=0;
+
+        solve(s,i,st,maxCount,currCount);
+        return maxCount;
+    }
+};
