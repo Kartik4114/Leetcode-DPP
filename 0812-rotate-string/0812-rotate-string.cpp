3ab78f06e41c381a1778b03851dee42ec6141ad2
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-11-03 09:23:38 +0530

    Time: 0 ms (100%), Space: 7.7 MB (55.81%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -9,17 +9,34 @@ public:
 
         for(int i=1;i<=n;i++){
             if(s==goal) return true;
+
             rotate(s.begin(),s.begin()+1,s.end());
         }
         return false;
     }
-    // bool a2(string &s,string &goal){
+    bool a2(string &s,string &goal){
         
-    // }
+        string temp=s;
+        temp+=s;
+
+        int m=s.size();
+        int n=goal.size();
+
+        if(m!=n) return false;
+
+        if(temp.find(goal)!=string::npos) return true;
+
+        return false;
+    }
     bool rotateString(string s, string goal) {
         
         // APPROACH 1:- BRUTE FORCE APPROACH (USING C++ STL)
-        return a1(s,goal);
+        // T.C :- O(n^2) , S.C :- O(n)
+        // return a1(s,goal);
+
+        // APPROACH 2:- OPTIMISED APPROACH
+        // T.C :- O(N) , S.C :- O(2*N)
+        return a2(s,goal); 
 
 
     }
