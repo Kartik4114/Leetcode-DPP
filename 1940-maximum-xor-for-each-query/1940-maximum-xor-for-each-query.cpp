71e617e144d7b787c68c2927b462ca4d297aae6e
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-11-08 15:34:25 +0530

    Time: 3 ms (67.62%), Space: 95.4 MB (63.99%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
-        
+    vector<int> a1(vector<int>& nums, int maximumBit){
         int n=nums.size();
         
         vector<int> xorArr(n);
@@ -30,7 +29,38 @@ public:
 
         reverse(result.begin(),result.end());
         return result;
+    }
+    vector<int> a2(vector<int>& nums, int maximumBit){
+        
+        int n=nums.size();
+        vector<int> result(n);
 
+        // Find the total XOR
+        int XOR = 0;
+        for(int i=0;i<n;i++){
+            XOR ^=nums[i];
+        }
+
+        // To find flip , firstly finding the mask of all bits set to 1
+        int mask= (1<<maximumBit) -1;
+
+        for(int i=0;i<n;i++){
+            int k=XOR ^ mask;
+
+            result[i]=k;
+            XOR = XOR ^ nums[n-i-1];
+        }
+        return result;
+    }
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        
+        // APPROACH 1:- MY APPROACH 
+        // T.C :- O(N) , S.C :- O(N)
+        // return a1(nums,maximumBit);
+
+        // APPROACH 2:- JUST LITTLE SIMPLE APPROACH (Actual approach)
+        // T.C :- O(N) , S.C :- O(N)
+        return a2(nums,maximumBit);
 
     }
 };
