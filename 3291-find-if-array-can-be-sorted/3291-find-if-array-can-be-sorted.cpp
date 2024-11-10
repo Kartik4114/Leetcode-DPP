165e999cbba6b6a743bd6ccbf156d0a5c5dc49e8
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-11-10 22:30:24 +0530

    Time: 27 ms (8.56%), Space: 31.8 MB (25.78%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    bool canSortArray(vector<int>& nums) {
-        
+    bool a1(vector<int> &nums){
         int n=nums.size();
 
         for(int i=0;i<n;i++){
@@ -20,5 +19,48 @@ public:
         }
 
         return true;
+    }
+
+    bool a2(vector<int> &nums){
+
+        int n=nums.size();
+
+        vector<pair<int,int>> vec;
+
+        int i=0;
+        while(i<n){
+
+            int minEl=nums[i];
+            int maxEl=nums[i];
+            
+            while(i+1<n && (__builtin_popcount(nums[i]) == __builtin_popcount(nums[i+1]))){
+                minEl=min(minEl,nums[i+1]);
+                maxEl=max(maxEl,nums[i+1]);
+                i++;
+            }
+
+            vec.push_back({minEl,maxEl});
+            i++;
+        }
+
+        for(int i=0;i<vec.size()-1;i++){
+
+            int currRegionMax=vec[i].second;
+            int nextRegionMin=vec[i+1].first;
+
+            cout<<currRegionMax<<" "<<nextRegionMin<<endl;
+            if(currRegionMax > nextRegionMin) return false;
+        }
+        return true;
+    }
+    bool canSortArray(vector<int>& nums) {
+        
+        // APPROACH 1:- USING BUBBLE SORT 
+        // T.C :- O(N^2) , S.C :- O(N)
+        // return a1(nums);
+
+        // APPROACH 2:- USING SEGMENT SORTING
+        // T.C :- O(N) 
+        return a2(nums);
     }
 };
