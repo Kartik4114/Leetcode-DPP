c6d54461a6b3d0fc69a2241d29403c7b6254533e
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-11-18 23:43:32 +0530

    Time: 0 ms (100%), Space: 10.3 MB (84%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -1,56 +1,29 @@
 class Solution {
 public:
-    vector<int> prevKSum(vector<int> &arr,int k){
-        
-        int n=arr.size();
-
-        vector<int> result;
-        for(int i=n-1;i>=n/2;i--){
-
-            int sumIdx=0;
-            for(int j=i-1;j>i+(k-1);j--){
-                sumIdx+=arr[j];
-            }
-            cout<<sumIdx<<" ";
-            result.push_back(sumIdx);
-        }
-
-        reverse(result.begin(),result.end());
-        return result;
-    }
-
-    vector<int> nextKSum(vector<int> &arr,int k){
-
-        int n=arr.size();
-
-        vector<int> result;
-        for(int i=0;i<n/2;i++){
-
-            int sumIdx=0;
-            for(int j=i+1;j<i+k+1;j++){
-                sumIdx+=arr[j];
-            }
-            result.push_back(sumIdx);
-        }
-
-        return result;
-    }
     vector<int> decrypt(vector<int>& code, int k) {
         
         int n=code.size();
 
-        vector<int> temp=code;
-        for(auto &c:code) temp.push_back(c);
+        vector<int> result(n,0);
 
-        if(k < 0){
-            return prevKSum(temp,k);
-        }
+        for(int i=0;i<n;i++){
 
-        else if(k==0){
-            vector<int> result(n,0);
-            return result;
+            if(k>0){
+                
+                for(int j=i+1;j<i+k+1;j++){
+                    result[i]+=code[j%n];
+                }
+
+            } else if(k==0) return result;
+
+            else {
+
+                for(int j=i-abs(k);j<i;j++){
+                    result[i]+=code[(j+n)%n];
+                }
+            }
+            
         }
-        
-        return nextKSum(temp,k);
+        return result;
     }
 };
