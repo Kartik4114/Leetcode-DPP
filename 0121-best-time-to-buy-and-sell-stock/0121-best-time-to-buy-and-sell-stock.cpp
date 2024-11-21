6ebac0e523dc58368d7413f852ea3e581f8d1060
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-11-21 23:56:35 +0530

    Time: 0 ms (100%), Space: 96.1 MB (72.42%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        
+    int a1(vector<int>& prices){
+
         int n=prices.size();
         int result=0;
 
@@ -15,6 +15,30 @@ public:
             }
         }
         return result;
+    }
+
+    int a2(vector<int>& prices){
+
+        int n=prices.size();
+        int maxProfit=0;
+        int mini=prices[0];
+
+        for(int i=1;i<n;i++){
+
+            int cost=prices[i]-mini;
+            maxProfit=max(maxProfit,cost);
+            mini=min(mini,prices[i]);
+
+        }
+        return maxProfit;
+    }
+    int maxProfit(vector<int>& prices) {
+        
+        // APPROACH 1:- MY APPROACH
+        // return a1(prices);
+
+        // APPROACH 2:-
+        return a2(prices);
         
     }
 };
