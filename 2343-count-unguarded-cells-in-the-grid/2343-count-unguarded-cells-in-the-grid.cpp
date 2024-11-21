e8d3717cdbbb8391c51b4d2dfd42b22838d67909
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-11-21 22:33:13 +0530

    Time: 53 ms (77.32%), Space: 172.2 MB (62.09%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
@@ -1,9 +1,40 @@
 class Solution {
 public:
+    void markGuarded(int row,int col,vector<vector<int>>& grid){
+        
+        int m=grid.size();
+        int n=grid[0].size();
+
+        // UP 
+        for(int i=row-1;i>=0;i--){
+            if(grid[i][col]==2 || grid[i][col]==3) break;
+            grid[i][col]=1;
+        }
+
+        // DOWN 
+        for(int i=row+1;i<m;i++){
+            if(grid[i][col]==2 || grid[i][col]==3) break;
+            grid[i][col]=1;
+        }
+
+        // LEFT
+        for(int j=col-1;j>=0;j--){
+            if(grid[row][j]==2 || grid[row][j]==3) break;
+            grid[row][j]=1;
+        }
+
+        // RIGHT
+        for(int j=col+1;j<n;j++){
+            if(grid[row][j]==2 || grid[row][j]==3) break;
+            grid[row][j]=1;
+        }
+    }
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
         
+        // T.C :- O(G*(r+c)) G=no of guards , r=no.of rows , c=no.of cols
         vector<vector<int>> vec(m,vector<int>(n,0));
 
+        // Wall
         for(auto &wall:walls){
 
             int x=wall[0];
@@ -11,49 +42,32 @@ public:
             
             vec[x][y]=2;
         }
+
+        // Guard
         for(auto &guard:guards){
 
             int x=guard[0];
             int y=guard[1];
             
-            vec[x][y]=2;
+            vec[x][y]=3;
         }
 
 
         for(auto &guard:guards){
-            int u=guard[0];
-            int v=guard[1];
+            int i=guard[0];
+            int j=guard[1];
 
-            for(int j=v+1;j<n;j++){
-                if(vec[u][j]==2) break;
-                vec[u][j]=1;
-            }
-            for(int j=v-1;j>=0;j--){
-                if(vec[u][j]==2) break;
-                vec[u][j]=1;
-            }
-
-            for(int i=u+1;i<m;i++){
-                if(vec[i][v]==2) break;
-                vec[i][v]=1;
-            }
-
-            for(int i=u-1;i>=0;i--){
-                if(vec[i][v]==2) break;
-                vec[i][v]=1;
-            }
+            markGuarded(i,j,vec);
         }
 
-        int result=0;
+        int count=0;
 
-        for(auto &row:vec){
-            for(auto &cell:row){
-                
-                if(cell==0)
-                result++;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(vec[i][j]==0) count++;
             }
         }
-        return result;
+        return count;
         
     }
 };
