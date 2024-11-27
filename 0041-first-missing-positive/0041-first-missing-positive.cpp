6e185b2d831ab0f10a9e11a97cff1cd4b442604a
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-11-27 19:39:06 +0530

    Time: 1 ms (67.14%), Space: 55.1 MB (24.91%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& arr) {
-        // using technique that no. can only be in [1,n] so using indexes as numbers
+    int firstMissingPositive(vector<int>& nums) {
+        
+        int n=nums.size();
 
-        int maxEl=*max_element(arr.begin(),arr.end());
-        
-        set<int> st(arr.begin(),arr.end());
-        
-        for(int i=1;i<=maxEl;i++){
+        bool contain=false;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==1) contain=true;
             
-            if(st.find(i)==st.end()) return i;
+            if(nums[i]<=0 || nums[i]>n) nums[i]=1;
         }
-        
-        if(maxEl<=0) return 1;
-        return maxEl+1;
+
+        if(contain==false) return 1;
+
+        for(int i=0;i<n;i++){
+
+            int num=abs(nums[i]);
+            int idx=num-1;
+
+            if(nums[idx]<0) continue;
+
+            nums[idx]*=-1;
+        }
+
+        for(int i=0;i<n;i++){
+            if(nums[i]>0) return i+1;
+        }
+        return n+1;
     }
 };
