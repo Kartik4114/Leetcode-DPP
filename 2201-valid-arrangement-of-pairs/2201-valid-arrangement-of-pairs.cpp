9fbf6aff121e00de36a0b33818cb44ea9f1d5c3b
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-12-01 00:43:35 +0530

    Time: 322 ms (87.14%), Space: 268.7 MB (76.27%) - LeetHub

--- 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.cpp
+++ 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.cpp
@@ -55,51 +55,67 @@ public:
         }
         return result;
     }
-    
-    //     vector<vector<int>> a1(vector<vector<int>>& pairs) {
 
-    //     unordered_map<int,vector<int>> adj;
-    //     vector<int,int> inDegree,outDegree;
+    vector<int> EulerPath;
+    void DFS(unordered_map<int,vector<int>> &adj,int u){
 
-    //     for(auto &pair:pairs){
-    //         int u=pair[0];
-    //         int v=pair[1];
+        while (!adj[u].empty()) {
+            int nextNode = adj[u].back();
+            adj[u].pop_back();  // Remove the edge after visiting
+            DFS(adj,nextNode);         // Recursive DFS call
+        }
+        EulerPath.push_back(u);  // Add node to Euler path after all edges are visited
 
-    //         adj[u].push_back(v);
-    //         inDegree[v]++;
-    //         outDegree[u]++;
-    //     }
+    }
+    vector<vector<int>> a2(vector<vector<int>>& pairs) {
 
-    //     // Find the startNode of the Euler Path
-    //     int startNode=pairs[0][0];
+        unordered_map<int,vector<int>> adj;
+        unordered_map<int,int> inDegree,outDegree;
 
-    //     for(auto &it:adj){
-    //         int node=it.first;
+        for(auto &pair:pairs){
+            int u=pair[0];
+            int v=pair[1];
 
-    //         if(outDegree[node]-inDegree[node]==1){
-    //             startNode=node;
-    //             break;
-    //         }
-    //     }
+            adj[u].push_back(v);
+            inDegree[v]++;
+            outDegree[u]++;
+        }
 
-    //     vector<int> EulerPath;
+        // Find the startNode of the Euler Path
+        int startNode=pairs[0][0];
+
+        for(auto &it:adj){
+            int node=it.first;
+
+            if(outDegree[node]-inDegree[node]==1){
+                startNode=node;
+                break;
+            }
+        }
         
-    //     // Doing DFS (Using Recursion)
+        // Doing DFS (Using Recursion)
+        DFS(adj,startNode);
 
-    //     reverse(EulerPath.begin(),EulerPath.end());
+        reverse(EulerPath.begin(),EulerPath.end());
 
-    //     vector<vector<int>> result;
+        vector<vector<int>> result;
 
-    //     for(int i=0;i<EulerPath.size()-1;i++){
-    //         result.push_back({EulerPath[i],EulerPath[i+1]});
-    //     }
-    //     return EulerPath;
-    // }
+        for(int i=0;i<EulerPath.size()-1;i++){
+            result.push_back({EulerPath[i],EulerPath[i+1]});
+        }
+        return result;
+    }
     
     vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
         
-        // APPROACH :- HEIRHODGER'S ALGORITHM
+        // APPROACH 1:- HEIRHODGER'S ALGORITHM
             // HERE WE ARE SOLVING USING DFS AS STACK
-        return a1(pairs);
+        // return a1(pairs);
+
+         // APPROACH 2:- HEIRHODGER'S ALGORITHM
+            // HERE WE ARE SOLVING USING DFS AS RECURSION
+        return a2(pairs);
+
+
     }
 };
