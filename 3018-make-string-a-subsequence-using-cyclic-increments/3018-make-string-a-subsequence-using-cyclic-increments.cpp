d2ace600249b3300228672c40073c7ef30038ed5
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-12-04 10:53:51 +0530

    Time: 2 ms (72.38%), Space: 17.1 MB (7.53%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -9,13 +9,12 @@ public:
         int j=0;
         while(i<n){
 
-            if(str1[i]==str2[j]){
-                i++;
-                j++;
-            } else if(char(str1[i]+1)==str2[j] || (str1[i]=='z' && str2[j]=='a')){
-                i++;
-                j++;
-            } else i++;
+            if(str1[i]== str2[j] || 
+                str1[i]+1==str2[j] || 
+                str1[i]-25==str2[j]) {
+                    j++;
+                }
+            i++;
         }
 
         if(j==m) return true;
