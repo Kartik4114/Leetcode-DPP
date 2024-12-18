1671e4e11e58db3b85cde7c0f816dd07b131ae4f
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-12-18 13:10:07 +0530

    Time: 2 ms (19.25%), Space: 14.2 MB (9.86%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
-    vector<int> finalPrices(vector<int>& prices) {
-        
+    vector<int> a1(vector<int> &prices){
         int n=prices.size();
-
         vector<int> result;
 
         for(int i=0;i<n;i++){
@@ -20,5 +18,32 @@ public:
         }
 
         return result;
+    }
+
+    vector<int> a2(vector<int> &prices){
+
+        int n=prices.size();
+
+        stack<int> st;
+        vector<int> result=prices;
+        for(int i=0;i<n;i++){
+
+            while(!st.empty() && prices[i]<=prices[st.top()]){
+                result[st.top()]-=prices[i];
+                st.pop();
+            }
+
+            st.push(i);
+        }
+
+        return result;
+    }
+    vector<int> finalPrices(vector<int>& prices) {
+        
+        // APPROACH 1:- BRUTE FORCE APPROACH
+        // return a1(prices);
+
+        // APPROACH 2:- OPTIMISED APPROACH (MONOTONIC STACK)
+        return a2(prices);
     }
 };
