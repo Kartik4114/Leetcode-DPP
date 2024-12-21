312242b4864a650239ea6ea2afb5e3454cd9b05a
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-12-21 18:33:51 +0530

    Time: 15 ms (83.89%), Space: 47.3 MB (20.37%) - LeetHub

---
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    int mod=1e9+7;
+
+    vector<int> getNSL(vector<int> &arr){
+        int n=arr.size();
+        vector<int> result(n);
+        stack<int> st;
+
+        for(int i=0;i<n;i++){
+
+            if(st.empty()){
+                result[i]=-1;
+            } else {
+
+                while(!st.empty() && arr[st.top()]>arr[i]){
+                    st.pop();
+                }
+
+                result[i] = st.empty() ? -1 : st.top();
+            }
+            st.push(i);
+        }
+
+        return result;
+    }
+
+    vector<int> getNSR(vector<int> &arr){
+        int n=arr.size();
+
+        vector<int> result(n);
+        stack<int> st;
+
+        for(int i=n-1;i>=0;i--){
+
+            if(st.empty()){
+                result[i]=n;
+            } else {
+
+                while(!st.empty() && arr[st.top()]>=arr[i]){
+                    st.pop();
+                }
+
+                result[i]=st.empty() ? n : st.top();
+
+            }
+            st.push(i);
+        }
+        return result;
+    }
+    int sumSubarrayMins(vector<int>& arr) {
+        int n=arr.size();
+
+        vector<int> NSL=getNSL(arr);
+        vector<int> NSR=getNSR(arr);
+        
+        long long result=0;
+
+        for(int i=0;i<n;i++){
+
+            long long ls=i-NSL[i];
+            long long rs=NSR[i]-i;
+
+            long long totalWays=ls*rs; // subarrays jinka minimum arr[i] hai
+
+            long long totalSum=arr[i]*totalWays;
+
+            result=(result+totalSum) % mod;
+
+        }
+
+        return result;
+
+    }
+};
