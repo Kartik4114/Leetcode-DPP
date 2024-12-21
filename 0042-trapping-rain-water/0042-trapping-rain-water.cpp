950aaf4ad9a551b6387d93ef3cc2af5f568fc95a
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-12-21 13:20:31 +0530

    Time: 3 ms (20.72%), Space: 26 MB (26.89%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -41,11 +41,48 @@ public:
 
         return result;
     }
+
+    int a2(vector<int> &height){
+
+        int n=height.size();
+        int left=0;
+        int right=n-1;
+
+        int leftMax=0,rightMax=0;
+
+        int result=0;
+        while(left<=right){
+
+            if(height[left]<=height[right]){
+
+                if(height[left]>=leftMax){
+                    leftMax=height[left];
+                } else {
+                    result+=leftMax-height[left];
+                }
+                left++;
+            } else {
+                if(height[right]>=rightMax){
+                    rightMax=height[right];
+                } else {
+                    result+=rightMax-height[right];
+                }
+
+                right--;
+            }
+
+        }
+        return result;
+    }
     int trap(vector<int>& height) {
         
         // APPROACH 1:-
         // T.C :- O(n) , S.C :- O(n)
-        return a1(height);
+        // return a1(height);
+
+        // APPROACH 2:- 
+        // T.C :- O(n) , S.C :-O(1)
+        return a2(height);
 
     }
 };
