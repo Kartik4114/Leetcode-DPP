0c0564484251c7e9f766360077db10e1637f62b5
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-12-24 11:42:37 +0530

    Time: 27 ms (64.57%), Space: 85.8 MB (29.61%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    vector<int> getPSE(vector<int> &arr){
+
+        int n=arr.size();
+        stack<int> st;
+
+        vector<int> result(n);
+        for(int i=0;i<n;i++){
+            if(st.empty()){
+                result[i]=-1;
+            } else {
+
+                while(!st.empty() && arr[st.top()]>=arr[i]){
+                    st.pop();
+                }
+
+                result[i]= st.empty() ? -1 : st.top();
+            }
+            st.push(i);
+        }
+
+        return result;
+    }
+    vector<int> getNSE(vector<int> &arr){
+
+        int n=arr.size();
+        stack<int> st;
+
+        vector<int> result(n);
+        for(int i=n-1;i>=0;i--){
+            if(st.empty()){
+                result[i]=n;
+            } else {
+
+                while(!st.empty() && arr[st.top()]>=arr[i]){
+                    st.pop();
+                }
+
+                result[i]= st.empty() ? n : st.top();
+            }
+            st.push(i);
+        }
+
+        return result;
+    }
+    int a1(vector<int> &heights){
+
+        int n=heights.size();
+
+        vector<int> NSE=getNSE(heights);
+        vector<int> PSE=getPSE(heights);
+
+        int maxi=-1;
+        for(int i=0;i<n;i++){
+
+            int currVal=heights[i]*(NSE[i]-PSE[i]-1);
+            maxi=max(maxi,currVal);
+        }
+
+        return maxi;
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        
+        // APPROACH 1:- It is a two pass solution
+        // T.C :- O(n) , S.C :-O(n)
+        return a1(heights);
+    }
+};
