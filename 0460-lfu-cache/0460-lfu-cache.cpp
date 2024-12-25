259a8094fb408f3a9fc681f894659e253aecee54
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-12-25 16:20:06 +0530

    Time: 145 ms (46.55%), Space: 197.5 MB (27.47%) - LeetHub

---
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -0,0 +1,82 @@
+class LFUCache {
+public:
+    int cap;
+    int size;
+    unordered_map<int,list<vector<int>> :: iterator> mp; // key -> address
+    map<int,list<vector<int>>> freq; // {key,value,counter}
+
+    LFUCache(int capacity) {
+        cap=capacity;
+        size=0;
+    }
+    
+    void makeMostFrequentlyUsed(int key) {
+
+        auto &vec = *(mp[key]);
+
+        int value= vec[1];
+        int f    = vec[2];
+
+        freq[f].erase(mp[key]);
+
+        if(freq[f].empty()) {
+            freq.erase(f);
+        }
+        f++;
+
+        freq[f].push_front({key,value,f});
+        mp[key]=freq[f].begin();
+
+    }
+    int get(int key) {
+        
+        if(mp.find(key)==mp.end()) return -1;
+
+        auto &vec=(*mp[key]); // extracting direct vector 
+
+        int value=vec[1];
+        makeMostFrequentlyUsed(key);
+
+        return value;
+    }
+    
+    void put(int key, int value) {
+        
+        if(mp.find(key)!=mp.end()){
+            auto &vec = *(mp[key]);
+            vec[1]=value;
+            makeMostFrequentlyUsed(key);
+        } else if(size<cap) {
+            
+            size++;
+            freq[1].push_front({key,value,1});
+            mp[key]=freq[1].begin();
+            // cap--;
+        }
+
+        else {
+            auto &listToDel=freq.begin()->second;
+
+            int keyToDel=(listToDel.back())[0];
+            listToDel.pop_back();
+
+            if(listToDel.empty()) {
+                freq.erase(freq.begin()->first);
+            }
+
+            freq[1].push_front({key,value,1});
+
+            mp.erase(keyToDel);
+            mp[key]=freq[1].begin();
+            // cap++;
+        }
+
+    }
+};
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache* obj = new LFUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
