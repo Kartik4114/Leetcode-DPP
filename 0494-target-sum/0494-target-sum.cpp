0317d200e29bf58df55e2464d12e3efc02d48729
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-12-26 13:57:16 +0530

    Time: 393 ms (32.77%), Space: 90 MB (6.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    int solve(vector<int> &nums,int target,int idx,int sum){
+    int solve(vector<int> &nums,int target,int idx,int sum,unordered_map<string,int> &memo){
         int n=nums.size();
 
         if(idx==n){
-            return (sum==target) ? 1 : 0;
+            return sum==target ? 1 : 0;
         }
+        string key = to_string(idx) + "," + to_string(sum);
+        if (memo.find(key) != memo.end()) {
+            return memo[key];
+        }
+        int add=solve(nums,target,idx+1,sum+nums[idx],memo);
+        int subtract=solve(nums,target,idx+1,sum-nums[idx],memo);
 
-        int add=solve(nums,target,idx+1,sum+nums[idx]);
-        int subtract=solve(nums,target,idx+1,sum-nums[idx]);
-
-        return add+subtract;
+        return memo[key]=add+subtract;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
 
-        return solve(nums,target,0,0);
+        // APPROACH 1:- Using recursion , O(2^n) (Working fine)
+        // Using Memoization O(n^2)
+
+        int n=nums.size();
+        unordered_map<string,int> memo;
+        return solve(nums,target,0,0,memo);
     }
 };
