976d4da643b28d0752c172fd9dfc55ea74f9c89a
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2024-12-26 22:44:11 +0530

    Time: 15 ms (59.97%), Space: 26.4 MB (25.06%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(vector<int> &nums,int target,int idx,int sum,unordered_map<string,int> &memo){
+    int solve1(vector<int> &nums,int target,int idx,int sum,unordered_map<string,int> &memo){
         int n=nums.size();
 
         if(idx==n){
@@ -10,18 +10,55 @@ public:
         if (memo.find(key) != memo.end()) {
             return memo[key];
         }
-        int add=solve(nums,target,idx+1,sum+nums[idx],memo);
-        int subtract=solve(nums,target,idx+1,sum-nums[idx],memo);
+        int add=solve1(nums,target,idx+1,sum+nums[idx],memo);
+        int subtract=solve1(nums,target,idx+1,sum-nums[idx],memo);
 
         return memo[key]=add+subtract;
+    }
+
+    int a1(vector<int>& nums, int target){
+
+        int n=nums.size();
+        unordered_map<string,int> memo;
+        return solve1(nums,target,0,0,memo);
+    }
+    int S;
+
+    int solve2(vector<int> &nums,int target,int idx,int sum,vector<vector<int>> &memo){
+        int n=nums.size();
+
+        if(idx==n){
+            return sum==target ? 1 : 0;
+        }
+        
+        if(memo[idx][sum+S]!=-1) return memo[idx][sum+S];
+
+        int add=solve2(nums,target,idx+1,sum+nums[idx],memo);
+        int subtract=solve2(nums,target,idx+1,sum-nums[idx],memo);
+
+        return memo[idx][sum+S]=add+subtract;
+    }
+    int a2(vector<int>& nums, int target){
+
+        int n=nums.size();
+        S=accumulate(nums.begin(),nums.end(),0);
+
+        vector<vector<int>> memo(n+1,vector<int>(2*S+1,-1));
+        return solve2(nums,target,0,0,memo);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
 
         // APPROACH 1:- Using recursion , O(2^n) (Working fine)
-        // Using Memoization O(n^2)
+        // Using Memoization (Using unordered map as memoization)
+        // T.C :- O(n^2) , S.C :- O(n^2)
 
-        int n=nums.size();
-        unordered_map<string,int> memo;
-        return solve(nums,target,0,0,memo);
+        // return a1(nums,target);
+
+
+        // APPROACH 2:-
+        // Using Memoization (Using 2-D array as memoization)
+         // T.C :- O(n^2) , S.C :- O(n^2)
+
+        return a2(nums,target);
     }
 };
