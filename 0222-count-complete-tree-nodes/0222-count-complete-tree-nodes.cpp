13766fbfb75bb21dfc7f93b448c374db153c68c3
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-01-01 22:39:04 +0530

    Time: 0 ms (100%), Space: 31.3 MB (27.07%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,29 +11,45 @@
  */
 class Solution {
 public:
+    int getLeftHeight(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        int lh = 0;
+        TreeNode* temp = root;
+        while(temp) {
+            temp = temp->left;
+            lh++;
+        }
+        return lh;
+    }
+    int getRightHeight(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        int rh = 0;
+        TreeNode* temp = root;
+        while(temp) {
+            temp = temp->right;
+            rh++;
+        }
+        return rh;
+    }
     int countNodes(TreeNode* root) {
         
+        // APPROACH 1:- BRUTE FORCE APPROACH (USING BFS OR DFS)
+        // T.C :- O(n)
+
+        // APPROACH 2:- OPTIMISED APPROACH (VALID ONly when complete binary tree is given)
+        // T.C :- O((logn)^2) 
         if(root==NULL) return 0;
 
-        queue<TreeNode*> que;
-        que.push(root);
+        int lh=getLeftHeight(root);
+        int rh=getRightHeight(root);
 
-        int count=0;
+        if(lh==rh) return pow(2,lh)-1;
 
-        while(!que.empty()){
+        return countNodes(root->left)+countNodes(root->right) +1;
 
-            int n=que.size();
-            count+=n;
-            while(n--){
-
-                auto curr=que.front();
-                que.pop();
-
-                if(curr->left) que.push(curr->left);
-                if(curr->right) que.push(curr->right);
-                
-            }
-        }
-        return count;
     }
 };
