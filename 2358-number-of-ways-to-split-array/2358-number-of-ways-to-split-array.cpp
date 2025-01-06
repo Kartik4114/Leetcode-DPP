1345b384c2974975c26189197c00b78aa7c4402a
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-01-07 01:30:08 +0530

    Time: 2 ms (58.55%), Space: 89.5 MB (37.84%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
-    static int waysToSplitArray(vector<int>& nums) {
-        const int n=nums.size();
-        long long sum=accumulate(nums.begin(), nums.end(), 0LL);
-        long long acc=0LL;
-        int cnt=0;
-        for(int i=0; i<n-1; i++){
-            acc+=nums[i];
-            cnt+=(2*acc>=sum);
+    int waysToSplitArray(vector<int>& nums) {
+        
+        int n=nums.size();
+
+        long long sum=0;
+
+        for(auto &num:nums){
+            sum+=num;
         }
-        return cnt;
+
+        long long leftSum=0;
+        long long rightSum=0;
+        int split=0;
+
+        for(int i=0;i<n-1;i++){
+
+            leftSum+=nums[i];
+
+            rightSum=sum-leftSum;
+            if(leftSum>=rightSum) split++;
+        }
+
+        return split;
+
     }
 };
