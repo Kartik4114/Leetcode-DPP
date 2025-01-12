fbfe3ba33041af57dd8dcf1d25ca7144bce7a58c
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-01-12 16:06:56 +0530

    Time: 15 ms (51.52%), Space: 34.7 MB (21.57%) - LeetHub

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool a1(string &s,string &locked){
+        
+        int n=s.size();
+        if(n&1) return false;
+
+        stack<int> open;
+        stack<int> openClose;
+
+        for(int i=0;i<n;i++){
+            
+            if(locked[i]=='0') {
+                openClose.push(i);
+            } else if(s[i]=='('){
+                open.push(i);
+            } else  {
+
+                if(!open.empty()){
+                    open.pop();
+                } else if(!openClose.empty()){
+                    openClose.pop();
+                } else return false;
+            }
+        }
+
+        while(!open.empty() && !openClose.empty() && open.top()<openClose.top()){
+            open.pop();
+            openClose.pop();
+        }
+
+        return open.empty() ? true : false;
+    }
+    // bool a2(string &s,string &locked){
+        
+    // }
+    bool canBeValid(string s, string locked) {
+        
+        // APPROACH 1:- USING STACKS
+        // T.C :- O(n), S.C :- O(n)
+        return a1(s,locked);
+
+        // APPROACH 1:- USING STACKS
+        // T.C :- O(n), S.C :- O(1)
+        // return a1(s,locked);
+
+
+    }
+};
