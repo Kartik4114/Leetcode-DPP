e227380f7477c7596df08b15985d02f81da29afa
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-01-17 01:27:19 +0530

    Time: 0 ms (100%), Space: 64.3 MB (64.43%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -8,21 +8,13 @@ public:
         int ans=0;
         if(n%2==0 && m%2==0) return 0;
 
-        else if(n%2==1 && m%2==1){
-
-            for(int i=0;i<n;i++){
-                ans^=nums1[i];
-            }
+        if(n%2==1){
 
             for(int i=0;i<m;i++){
                 ans^=nums2[i];
             }
-        } else if(n%2==1){
-
-            for(int i=0;i<m;i++){
-                ans^=nums2[i];
-            }
-        } else if(m%2==1){
+        }
+        if(m%2==1){
 
             for(int i=0;i<n;i++){
                 ans^=nums1[i];
