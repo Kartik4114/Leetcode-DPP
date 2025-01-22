862e60e45be7184293fe60a5e3959539fd66bce3
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-01-23 00:40:50 +0530

    Time: 85 ms (47.94%), Space: 112.9 MB (75.24%) - LeetHub

---
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<vector<int>> directions = {{-1,0},{1,0},{0,-1},{0,1}};
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+    //  if (isWater.empty() || isWater[0].empty()) return {};
+
+        int m=isWater.size();
+        int n=isWater[0].size();
+
+        vector<vector<int>> heights(m,vector<int>(n,-1));
+        queue<pair<int,int>> que;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                if(isWater[i][j]==1){
+                    heights[i][j]=0;
+                    que.push({i,j});
+                }
+            }
+        }
+
+        while(!que.empty()){
+
+            int levelSize=que.size();
+
+            while(levelSize--){
+
+                auto P=que.front();
+                que.pop();
+
+                int i=P.first;
+                int j=P.second;
+
+                for(auto &dir:directions){
+
+                    int new_i=dir[0]+i;
+                    int new_j=dir[1]+j;
+
+                    if(new_i>=0 && new_i<m && new_j>=0 && new_j<n && heights[new_i][new_j]==-1){
+                        heights[new_i][new_j]=heights[i][j]+1;
+                        que.push({new_i,new_j});
+                    }
+                }
+            }
+        }
+
+        return heights;
+    }
+};
