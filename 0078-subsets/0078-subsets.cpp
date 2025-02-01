da83916b7ebc6fb513f85c6980a7a61c46895ebc
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-02-01 15:50:57 +0530

    Time: 0 ms (100%), Space: 10.1 MB (41.97%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,46 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void a1(vector<int> &nums,int i,vector<int> temp){
-        if(i>=nums.size()){
+    void solve(vector<int> &nums,int idx,vector<int> &temp){
+        int n=nums.size();
+        if(idx>=n){
             ans.push_back(temp);
             return;
         }
 
-        temp.push_back(nums[i]);
-        a1(nums,i+1,temp);
+        temp.push_back(nums[idx]);
+        solve(nums,idx+1,temp);
         temp.pop_back();
-
-        a1(nums,i+1,temp);
-        
-    }
-
-    vector<vector<int>> a2(vector<int> &nums){
-        // counting size of subsets
-        int sub_size=pow(2,nums.size());
-        vector<vector<int>> result;
-
-        for(int i=0;i<sub_size;i++){
-            vector<int> temp;
-
-            for(int j=0;j<nums.size();j++){
-                if(i & (1<<j)) temp.push_back(nums[j]);
-            }
-            result.push_back(temp);
-        }
-
-        return result;
+        solve(nums,idx+1,temp);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        // APPROACH 1:- USING RECURSION 
-        // T.C :- O(2^n) , S.C:- O(2^n)
-            // vector<int> temp;
-            // a1(nums,0,temp);
-
-            // return ans;
         
-        // APPROACH 2:- USING BIT MANIPULATION
-        // T.C :- O(n*(2^n)) , S.C:- O(n*(2^n))
-            return a2(nums);
+        vector<int> temp;
+        solve(nums,0,temp);
+        return ans;
     }
 };
