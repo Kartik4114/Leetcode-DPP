99bfdeae547010cd0a3b2b399e20a33fff3ba778
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-02-01 13:11:58 +0530

    Time: 0 ms (100%), Space: 27.6 MB (86.97%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    bool isArraySpecial(vector<int>& arr) {
+    bool isArraySpecial(vector<int>& nums) {
         
-        // bool check=true;
-        
-        for(int i=0;i<arr.size()-1;i++){
-            
-            if(((arr[i]%2==1) && (arr[i+1]%2==1)) || ((arr[i]%2==0) && (arr[i+1]%2==0)))  return false;
+        int n=nums.size();
+
+        if(n<=1) return true;
+
+        for(int i=0;i<n-1;i++){
+            if((nums[i]&1)==(nums[i+1]&1)) return false;
         }
-        
+
         return true;
     }
 };
