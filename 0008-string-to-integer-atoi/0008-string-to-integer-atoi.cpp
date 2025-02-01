1c7f38e77d195e23ebe40346103e4d0599aeda9e
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-02-01 14:08:34 +0530

    Time: 15 ms (5.15%), Space: 9.2 MB (58%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,61 +1,36 @@
 class Solution {
 public:
-
-    long long num;
-    void solve(string &temp,int idx ,bool sign){
-        int n=temp.size();
-        if(idx>=n){
-            return;
-        }
-        
-        num=num*10+(temp[idx]-'0');
-        if(num>=INT_MAX && sign==false){
-            num= INT_MAX;
-            return;
-        }
-        if(-num<=INT_MIN && sign==true){
-            num= INT_MIN;
-            return;
-        }
-
-        solve(temp,idx+1,sign);
-    }
     int myAtoi(string s) {
         
-        if(s.size()==0) return 0;
-        string temp;
+        int n=s.size();
+
         int idx=s.find_first_not_of(" ");
-        
+
         if(idx<0) return 0;
-        cout<<idx<<endl;
-        bool sign =false;
+
+        bool sign=false;
+
         if(s[idx]=='-') sign=true;
         if(s[idx]=='-' || s[idx]=='+') idx++;
+        
 
+        long long ans=0;
         for(int i=idx;i<s.size();i++){
 
             if(s[i]>='0' && s[i]<='9'){
-                temp.push_back(s[i]);
-            }
 
-            else break;
+                ans=(long long)ans*10+(s[i]-'0');
+
+                if(sign==false){
+                    if(ans>=INT_MAX) return INT_MAX;
+                } else {
+                    if(-ans<=INT_MIN) return INT_MIN;
+                }
+                cout<<ans<<endl;
+            } else break;
         }
 
-        int n=temp.size();
-        
-        if(n==0) return 0;
-        num=0;
-        solve(temp,0,sign);
+        return sign==true ? -ans : ans;
 
-
-
-        if(num==INT_MIN && sign==true) return INT_MIN;
-        if(num==INT_MAX && sign ==false) return INT_MAX;
-
-        if(sign==true){
-            return -num;
-        }
-
-        return num;
     }
 };
