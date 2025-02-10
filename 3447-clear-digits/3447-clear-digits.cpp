b0ff354c1ec1a02109e997761e18e81b881c54d8
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-02-10 14:27:13 +0530

    Time: 0 ms (100%), Space: 8.5 MB (70.42%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
+    string a1(string &s){
+
+        // int n=s.size(); // Don't do this because s.length() is changing every time 
+
+        int i=0;
+        while(i<s.size()){
+            if(isdigit(s[i])){
+                s.erase(i,1);
+                
+                if(i>=0){
+                    s.erase(i-1,1);
+                }
+                i--;
+            } else i++;
+        }
+        return s;
+
+    }
     string clearDigits(string s) {
         
-        int n=s.size();
-        stack<char> st;
-
-        for(auto &ch:s){
-
-            if(!st.empty() && ch>='0' && ch<='9'){
-                st.pop();
-            } else {
-                st.push(ch);
-            }
-        }
-
-        string result="";
-        while(!st.empty()){
-            result+=st.top();
-            st.pop();
-        }
-
-        reverse(result.begin(),result.end());
-        return result;
+        // APPROACH 1:- BRUTE FORCE APPROACH
+        return a1(s);
     }
 };
