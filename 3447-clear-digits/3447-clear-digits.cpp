1dbd9905fbe7b5e5906ed8fe3e13fe6c247287ac
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-02-10 14:31:46 +0530

    Time: 2 ms (20.65%), Space: 9 MB (41.76%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -18,9 +18,40 @@ public:
         return s;
 
     }
+
+    string a2(string &s){
+
+        int n=s.size();
+        stack<char> st;
+
+        for(auto &ch:s){
+
+            if(!st.empty() && isdigit(ch)){
+                st.pop();
+            } else {
+                st.push(ch);
+            }
+        }
+
+        string result="";
+        while(!st.empty()){
+            result+=st.top();
+            st.pop();
+        }
+
+        reverse(result.begin(),result.end());
+        return result;
+    }
     string clearDigits(string s) {
         
         // APPROACH 1:- BRUTE FORCE APPROACH
-        return a1(s);
+        // T.C :- O(n^2)
+        // return a1(s);
+
+        // APPROACH 2:- USING STACK
+        // T.C :- O(n) , S.C :- O(n)
+        return a2(s);
+
+
     }
 };
