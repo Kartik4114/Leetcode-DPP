23c07d0ad525483c06eacd3c9d56bec5a332b856
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-03-28 02:21:33 +0530

    Time: 27 ms (63.07%), Space: 98.7 MB (43.98%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int findDominantEl(vector<int> &nums) {
+        unordered_map<int, int> mp;
+
+        for (auto &num : nums) {
+            mp[num]++;
+        }
+
+        for (auto &it : mp) {
+            if (it.second > nums.size() / 2) {
+                return it.first;
+            }
+        }
+
+        return -1;
+    }
+
+    int minimumIndex(vector<int> &nums) {
+        int n = nums.size();
+
+        int el = findDominantEl(nums);
+        if (el == -1) return -1;
+
+        int majElCnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el)
+                majElCnt++;
+        }
+
+        int majElCurrCnt = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == el)
+                majElCurrCnt++;
+
+            if (majElCurrCnt > (i + 1)/2 && (majElCnt - majElCurrCnt) > (n - i - 1)/2) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
