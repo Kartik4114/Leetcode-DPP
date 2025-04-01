39ad8b2a865233fd4eb1d066311712b9a5524d51
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-04-01 23:43:36 +0530

    Time: 715 ms (5.22%), Space: 119.6 MB (86.16%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        
+        int n=questions.size();
+
+        vector<long long> dp(n,0);
+        dp[n-1]=questions[n-1][0];
+
+        // cout<<dp[n-1]<<endl;
+        long long maxVal=dp[n-1];
+        for(int i=n-2;i>=0;i--){
+            
+            int skip=questions[i][1];
+            if(skip+i+1<n){
+                dp[i]=std::max(questions[i][0]+dp[i+1+skip],maxVal);
+            } else {
+                dp[i]=std::max(maxVal,(long long)questions[i][0]);
+            }
+
+            cout<<dp[i]<<" ";
+            maxVal=max(maxVal,dp[i]);
+        }
+
+        return maxVal;
+    }
+};
