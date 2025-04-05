4e357543d0d318c7b01b0a98e54d0fbdc7766017
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-04-05 22:37:22 +0530

    Time: 0 ms (100%), Space: 9.2 MB (42.82%) - LeetHub

---
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dfs(vector<int>& nums,int idx,int currXor){
+
+        if(idx>=nums.size()){
+            return currXor;
+        }
+
+        int include=dfs(nums,idx+1,currXor^nums[idx]);
+        int exclude=dfs(nums,idx+1,currXor);
+
+        return include+exclude;
+    }
+    int subsetXORSum(vector<int>& nums) {
+        
+        return dfs(nums,0,0);
+    }
+};
