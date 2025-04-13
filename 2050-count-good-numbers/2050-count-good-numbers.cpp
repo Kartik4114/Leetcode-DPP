9c9a4b9db9684e7d761bdddc12927645a82a8915
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-04-13 18:21:10 +0530

    Time: 0 ms (100%), Space: 7.8 MB (96.45%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    int mod = 1e9+7;
+    const int mod=1e9+7;
+    int findPower(long long x,long long power){
+        if(power==0) return 1;
 
-    long long modPow(long long x, long long power) {
-        if (power == 0) return 1;
+        long long halfPower=findPower(x,power/2);
+        long long result=(halfPower*halfPower)%mod;
 
-        long long halfPower = modPow(x, power / 2);
-        halfPower = (halfPower * halfPower) % mod;
-
-        if (power % 2 == 1) {
-            return (halfPower * x) % mod;
+        if(power&1){
+            return (x*result)%mod;
         }
-        return halfPower;
+        return result;
     }
-
     int countGoodNumbers(long long n) {
-        long long pairs = n / 2;
-        long long ans = modPow(20, pairs);
-
-        if (n % 2 == 1) {
-            ans = (ans * 5) % mod;
-        }
-        return ans;
+        return (long long) findPower(5,(n+1)/2)*findPower(4,n/2) %mod;
     }
 };
