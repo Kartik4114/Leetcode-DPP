9019e4eb9e32ec6362bb96c3c3b6a86c8d480c2e
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-04-18 23:43:42 +0530

    Time: 4 ms (56.36%), Space: 10.2 MB (38.38%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string helper(int n){
+
+        if(n==1) return "1";
+
+        string say=helper(n-1);
+
+        string result="";
+
+        for(int i=0;i<say.size();i++){
+            
+            char ch=say[i];
+            int cnt=1;
+            while(i<say.size()-1 && say[i]==say[i+1]){
+                cnt++;
+                i++;
+            }
+
+            result+=to_string(cnt);
+            result+=ch;
+        }
+
+        return result;
+
+    }
+    string countAndSay(int n) {
+        
+        string result=helper(n);
+        return result;
+    }
+};
