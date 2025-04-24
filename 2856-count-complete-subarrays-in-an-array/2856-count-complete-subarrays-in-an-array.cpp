d5471b834178dc1746c539751ef540378b368925
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-04-24 14:41:45 +0530

    Time: 20 ms (60.91%), Space: 41.9 MB (59.44%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
-        int n = nums.size();
-        unordered_set<int> st(nums.begin(), nums.end());
-        int totalDistinct = st.size();
 
-        int result = 0;
-        unordered_map<int, int> mp;
+        int n=nums.size();
+        unordered_set<int> st(nums.begin(),nums.end());
+         int totalDistinct = st.size();
+        
+        int result=0;
+        unordered_map<int,int> mp;
 
-        int i = 0, j = 0;
+        int i=0;
+        int j=0;
+        
+        while(i<n){
 
-        while (i < n) {
-            while (j < n && mp.size() < totalDistinct) {
+            while(j<n && mp.size()<totalDistinct){
                 mp[nums[j]]++;
                 j++;
             }
@@ -21,10 +24,12 @@ public:
             }
 
             mp[nums[i]]--;
-            if (mp[nums[i]] == 0) mp.erase(nums[i]);
+            if(mp[nums[i]]==0) mp.erase(nums[i]);
             i++;
+
         }
 
         return result;
+
     }
 };
