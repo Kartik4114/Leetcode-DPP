44937cc193fbf92c8b7529e78082981eaaf5d5e3
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-05-06 10:29:05 +0530

    Time: 0 ms (100%), Space: 20.4 MB (58.11%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    vector<int> buildArray(vector<int>& nums) {
-        
+
+    vector<int> a1(vector<int>& nums){
+
         int n=nums.size();
         vector<int> ans(n);
 
@@ -10,7 +11,33 @@ public:
         }
 
         return ans;
+    }
 
+    vector<int> a2(vector<int> &nums){
+
+        int n=nums.size();
+
+        for(int i=0;i<n;i++){
+
+            nums[i]=nums[i]+n*(nums[nums[i]]%n);
+        }
+
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]/n;
+        }
+
+        return nums;
+    }
+    vector<int> buildArray(vector<int>& nums) {
         
+        // Approach 1:- simple approach
+        // T.C :- O(n) , S.C :- O(n)
+
+        // return a1(nums);
+
+        // Approach 2:- Optimised Approach (Nice Follow Up Question)
+        // T.C :- O(n) , S.C :- O(1)
+
+        return a2(nums);
     }
 };
