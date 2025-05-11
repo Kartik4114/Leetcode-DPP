bd88039a19af6f47bee72c22e27d23a66fc854ae
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-05-11 11:15:44 +0530

    Time: 0 ms (100%), Space: 11.9 MB (59.6%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -7,8 +7,9 @@ public:
         if(arr.size()<3) {
             return false;
         }
+        
         for(int i = 0; i <= arr.size() - 3; i++) {
-            if(arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1) {
+            if(arr[i] &1 && arr[i + 1] &1 && arr[i + 2] &1) {
                 return true;
             }
         }
