012c92a6f81746b9a81733bafab9ae4438227e12
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-05-12 16:51:02 +0530

    Time: 0 ms (100%), Space: 12.3 MB (99.4%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    vector<int> findEvenNumbers(vector<int>& digits) {
-        
+    vector<int> a1(vector<int>& digits){
         int n=digits.size();
 
         set<int> st;
@@ -30,6 +29,56 @@ public:
         }
 
         return result;
+
+    }
+
+    vector<int> a2(vector<int> &digits){
+
+        int n=digits.size();
+
+        vector<int> vec(10,0);
+        vector<int> result;
+
+        for(auto &digit:digits){
+            vec[digit]++;
+        }
+
+        for(int i=1;i<=9;i++){
+            if(vec[i]==0) continue;
+            vec[i]--;
+
+            for(int j=0;j<=9;j++){
+                if(vec[j]==0) continue;
+                vec[j]--;
+
+                for(int k=0;k<=8;k+=2){
+                    
+                    if(vec[k]==0) continue;
+                    vec[k]--;
+
+                    int num=i*100+j*10+k;
+                    result.push_back(num);
+                    vec[k]++;
+                }
+                vec[j]++;
+                
+            }
+            vec[i]++;
+        }
+
+        return result;
+    }
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        
+        // APPROACH 1:- USING BRUTE FORCE APPROACH
+        // T.C :- O(n^3) , S.C :- O(S)
+
+        // return a1(digits);
+
+        // APPROACH 2:- USING OPTIMISED APPROACH
+        // T.C :- O(1) , S.C :- O(1)
+
+        return a2(digits);
 
 
     }
