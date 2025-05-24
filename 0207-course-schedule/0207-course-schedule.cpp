5d7ee12646ab7c8d212b9d1153d0a23cb750e532
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-05-24 14:11:12 +0530

    Time: 8 ms (39.74%), Space: 20 MB (49.79%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,94 +1,58 @@
 class Solution {
 public:
-    bool BFS(int n,unordered_map<int,vector<int>> &adj,vector<int> &inDegree){
-        queue<int> que;
+    bool check(unordered_map<int,vector<int>> &adj,int N){
+
+        vector<int> inDegree(N);
         int cnt=0;
-        for(int i=0;i<n;i++){
-            if(inDegree[i]==0){
-                que.push(i);
-                cnt++;
+
+        for(int u=0;u<N;u++){
+            for(auto &v:adj[u]){
+                inDegree[v]++;
+                
+            }
+        }
+
+        queue<int> que;
+
+        for (int i = 0; i < N; ++i) {      // iterate by index
+            if (inDegree[i] == 0) {
+                que.push(i);               // ✔ push the vertex ID
             }
         }
 
         while(!que.empty()){
-            int u=que.front();
-            que.pop();
 
-            for(auto &v:adj[u]){
+            int node=que.front();
+            que.pop();
+            cnt++;
+            
+
+            for(auto &v:adj[node]){
 
                 inDegree[v]--;
-                if(inDegree[v]==0){
+                if(inDegree[v]==0) {
                     que.push(v);
-                    cnt++;
+                    // cnt++;
                 }
             }
         }
 
-        if(cnt==n) return true;
+        if(cnt==N) return true;
         return false;
-    }
-    bool a1(int numCourses, vector<vector<int>>& prerequisites){
-        unordered_map<int,vector<int>> adj;
-
-        vector<int> inDegree(numCourses,0);
-
-        for(auto &vec:prerequisites){
-            int a=vec[0];
-            int b=vec[1];
-
-            adj[b].push_back(a);
-            inDegree[a]++;
-        }
-
-        return BFS(numCourses,adj,inDegree);
-    }
-    
-    bool isCycleDFS(unordered_map<int,vector<int>> &adj,vector<bool> &visited,int u,
-                    vector<bool> &inRecursion){
-        visited[u]=true;
-        inRecursion[u]=true;
-
-        for(auto &v:adj[u]){
-            if(!visited[v] && isCycleDFS(adj,visited,v,inRecursion))
-            return true;
-
-            else if(inRecursion[v]==true) return true;
-        }
-        inRecursion[u]=false;
-        return false;
-
-    }
-    bool a2(int numCourses, vector<vector<int>>& prerequisites){
-        unordered_map<int,vector<int>> adj;
-
-        vector<bool> visited(numCourses,false);
-        vector<bool> inRecursion(numCourses,false);
-
-        for(auto &vec:prerequisites){
-            int a=vec[0];
-            int b=vec[1];
-
-            adj[b].push_back(a);
-        }
-
-        for(int i=0;i<numCourses;i++){
-
-            if(!visited[i] && isCycleDFS(adj,visited,i,inRecursion))
-             return false;
-        }
-        return true;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         
-        // APPROACH 1:- USING BFS
-        // IT IS DETECT CYCLE IN DIRECTED GRAPH(KAHN'S ALGORITHM)
+        unordered_map<int,vector<int>> adj;
 
-        // return a1(numCourses,prerequisites);
+        for(auto &edge:prerequisites){
 
-        // APPROACH 2:- USING DFS
+            int u=edge[0];
+            int v=edge[1];
 
-        // IT IS DETECT CYCLE IN DIRECTED GRAPH
-        return a2(numCourses,prerequisites);
+            adj[u].push_back(v);
+        }
 
+        bool ans=check(adj,numCourses);
+        return ans;
     }
 };
