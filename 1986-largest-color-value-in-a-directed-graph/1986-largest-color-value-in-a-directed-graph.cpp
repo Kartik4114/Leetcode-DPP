df4afc8584eadda7266aa7b94e3fb3da6857617e
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-05-26 23:57:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.cpp
+++ 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int largestPathValue(string colors, vector<vector<int>>& edges) {
         
-        // we will solve this question using topological Sort
-        unordered_map<int,vector<int>> adj;
-
         int n=colors.size();
-        vector<int> inDegree(n,0);
+
+        unordered_map<int,vector<int>> adj;
+        vector<int> inDegree()
 
         for(auto &edge:edges){
-            
+
             int u=edge[0];
             int v=edge[1];
 
@@ -17,39 +16,33 @@ public:
             inDegree[v]++;
         }
 
+        queue<int> que;
         vector<vector<int>> t(n,vector<int>(26,0));
 
-        queue<int> que;
         for(int i=0;i<n;i++){
             if(inDegree[i]==0){
-
-                que.push(i);
-                t[i][colors[i]-'a']=1;
+                que.push(d);
+                t[colors[i]-'a']=1;
             }
         }
 
-        int answer=0;
+        int result=0;
         int countNodes=0;
+
         while(!que.empty()){
 
             int u=que.front();
             que.pop();
 
+            result=max(result,t[u][colors[u]-'a']);
             countNodes++;
-            answer=max(answer,t[u][colors[u]-'a']);
 
             for(auto &v:adj[u]){
 
                 for(int i=0;i<26;i++){
-                    t[v][i]=max(t[v][i],t[u][i]+(colors[v]-'a'==i));
+                    
                 }
-                inDegree[v]--;
-
-                if(inDegree[v]==0) que.push(v);
             }
         }
-
-        if(countNodes<n) return -1;
-        return answer;
     }
 };
