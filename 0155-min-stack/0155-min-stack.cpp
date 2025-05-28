f96cd1d68abdb482bedfb085dcda1e0f193fe0e0
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-05-28 20:04:02 +0530

    Time: 4 ms (46.02%), Space: 23.1 MB (92.2%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,86 +1,41 @@
 class MinStack {
 public:
-    // APPROACH 1:- USING PAIR IN A STACK
-    // T.C :- O(N) , S.C :- O(2*N)
-
-    // stack<pair<int,int>> st;
-    // MinStack() {
-        
-    // }
-    
-    // void push(int val) {
-        
-    //     if(st.empty()) st.push({val,val});
-    //     else {
-            
-    //         int currMin=st.top().second;
-    //         st.push({val,min(val,currMin)});
-    //     }
-    // }
-    
-    // void pop() {
-        
-    //     st.pop();
-    // }
-    
-    // int top() {
-        
-    //     return st.top().first;
-    // }
-    
-    // int getMin() {
-    //     return st.top().second;
-    // }
-
-    // APPROACH 2:- WITHOUT USING PAIR IN STACK (OPTIMISED IN SPACE)
-    // T.C :- O(N) , S.C :- O(N)
-
-    stack<long long> st;
-    long long minEl=INT_MAX;
+    stack<pair<int,int>> st;
     MinStack() {
         
     }
     
     void push(int val) {
         
-        if(st.empty()){
-            st.push(val);
-            minEl=val;
-        } else {
-
-            if(val<minEl) {
-
-                long long newVal=2*1LL*val-minEl;
-                st.push(newVal);
-                minEl=val;
-            } else {
-
-                st.push(val);
-            }
+        int minEl=val;
+        if(!st.empty()){
+            minEl=min(minEl,st.top().second);
         }
+
+        st.push({val,minEl});
     }
     
     void pop() {
         
-        long long top=st.top();
-        st.pop();
-
-        if(top<minEl) {
-            minEl=2*minEl - top;
-        } 
+        if(!st.empty()){
+            st.pop();
+        }
     }
     
     int top() {
         
-        if(st.top()<minEl){
-            return minEl;
+        if(!st.empty()){
+            return st.top().first;
         }
-
-        return st.top();
+        return 0;
     }
     
     int getMin() {
-        return minEl;
+        
+        if(!st.empty()){
+            return st.top().second;
+        }
+        return 0;
     }
 };
 
