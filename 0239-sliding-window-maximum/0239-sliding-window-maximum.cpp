d36efcf1f7aa7c4c570b9bbfbe120e383b355309
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-05-31 00:33:25 +0530

    Time: 22 ms (66.15%), Space: 139.3 MB (38.36%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
         int n=nums.size();
 
-        deque<int> deq;
+        deque<int> dq;
         vector<int> result;
 
         for(int i=0;i<n;i++){
 
-            while(!deq.empty() && deq.front()<=i-k){
-                deq.pop_front();
+            while(!dq.empty() && dq.front()<=i-k){
+                dq.pop_front();
             }
 
-            while(!deq.empty() && nums[i]>nums[deq.back()]){
-                deq.pop_back();
+            while(!dq.empty() && nums[i]>nums[dq.back()]){
+                dq.pop_back();
             }
 
-            deq.push_back(i);
+            dq.push_back(i);
 
-            if(i>=k-1) {
-                result.push_back(nums[deq.front()]);
-            }
-            
+            if(i>=(k-1))
+            result.push_back(nums[dq.front()]);
         }
 
         return result;
-
     }
 };
