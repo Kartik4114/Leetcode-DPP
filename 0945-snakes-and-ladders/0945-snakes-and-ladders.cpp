4d069c9cccf5bbf3383fcd4b35ed79b7df21ec99
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-05-31 18:33:44 +0530

    Time: 4 ms (50.45%), Space: 16.7 MB (77.16%) - LeetHub

---
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int n;
+    pair<int,int> getCoords(int num){
+
+        int RT=(num-1)/n;
+        int RB=(n-1)-RT;
+
+        int col=(num-1)%n;
+        if(n%2==RB%2){
+
+            col=(n-1)-col;
+        }
+
+        return make_pair(RB,col);
+    }
+    int snakesAndLadders(vector<vector<int>>& board) {
+        
+        n=board.size();
+
+        int steps=0;
+
+        queue<int> que; 
+        vector<vector<int>> visited(n,vector<int>(n,0));
+        visited[n-1][0]=true;
+        que.push(1);
+
+        while(!que.empty()){
+
+            int N=que.size();
+            while(N--){
+
+                int x=que.front();
+                que.pop();
+
+                if(x==n*n) return steps;
+
+                for(int k=1;k<=6;k++){
+
+                    if(x+k>n*n) break;
+                    
+                    pair<int,int> coord=getCoords(x+k);
+
+                    int r=coord.first;
+                    int c=coord.second;
+
+                    if(visited[r][c]) continue;
+                    visited[r][c]=true;
+
+                    if(board[r][c]==-1){
+                        que.push(x+k);
+                    } else {
+                        que.push(board[r][c]);
+                    }
+                }
+            }
+            steps++;
+        }
+
+        return -1;
+    }
+};
