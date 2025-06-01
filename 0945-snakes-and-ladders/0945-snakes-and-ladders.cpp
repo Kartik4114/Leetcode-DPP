51c0e79d480d3c496845909fcc6366382578a4da
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-06-01 12:15:51 +0530

    Time: 0 ms (100%), Space: 17 MB (63.92%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.cpp
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.cpp
@@ -1,45 +1,51 @@
 class Solution {
 public:
     int n;
+
     pair<int,int> getCoords(int num){
 
         int RT=(num-1)/n;
         int RB=(n-1)-RT;
 
         int col=(num-1)%n;
-        if(n%2==RB%2){
 
-            col=(n-1)-col;
+        if(n%2==RB%2){
+            col = (n-1)-col;
         }
 
         return make_pair(RB,col);
     }
     int snakesAndLadders(vector<vector<int>>& board) {
-        
         n=board.size();
 
         int steps=0;
 
-        queue<int> que; 
+        queue<int> que;
+
         vector<vector<int>> visited(n,vector<int>(n,0));
         visited[n-1][0]=true;
         que.push(1);
 
+
+        // starting with the BFS code
         while(!que.empty()){
 
             int N=que.size();
+
             while(N--){
 
                 int x=que.front();
                 que.pop();
 
-                if(x==n*n) return steps;
+                if(x==n*n){
+                    return steps;
+                }
 
                 for(int k=1;k<=6;k++){
 
                     if(x+k>n*n) break;
-                    
-                    pair<int,int> coord=getCoords(x+k);
+
+                    auto coord=getCoords(x+k);
 
                     int r=coord.first;
                     int c=coord.second;
@@ -47,16 +53,17 @@ public:
                     if(visited[r][c]) continue;
                     visited[r][c]=true;
 
-                    if(board[r][c]==-1){
-                        que.push(x+k);
-                    } else {
+                    if(board[r][c]!=-1){
                         que.push(board[r][c]);
+                    } else {
+                        que.push(x+k);
                     }
                 }
             }
+
             steps++;
         }
 
-        return -1;
+        return -1;   
     }
 };
