9f3f57eef681e08429c68220b67091d10a3a7aaf
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-06-05 01:27:56 +0530

    Time: 50 ms (70.37%), Space: 10.4 MB (90.24%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -5,7 +5,7 @@ public:
         int n=word.size();
 
         if(numFriends==1) return word;
-        
+
         string result;
         int largestPossible=n-(numFriends-1);
 
@@ -17,15 +17,51 @@ public:
 
         return result;
     }
+
+    string a2(string word,int numFriends){
+
+        int n = word.size();
+        if (numFriends == 1) return word;
+
+        int i = 0, j = 1;
+
+        while (j < n) {
+            while (j < n && word[j] < word[i]) {
+                j++;
+            }
+
+            if (j < n && word[j] > word[i]) {
+                i = j;
+                j = i + 1;
+            } else if (j < n && word[j] == word[i]) {
+                int k = 1;
+                while (j + k < n && word[i + k] == word[j + k]) {
+                    k++;
+                }
+
+                if (j + k < n && word[i + k] < word[j + k]) {
+                    i = j;
+                    j = i + 1;
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        int longestPossibleLength = n - (numFriends - 1);
+        int canTakePossible = min(longestPossibleLength, n - i);
+
+        return word.substr(i, canTakePossible);
+    }
     string answerString(string word, int numFriends) {
         
         // APPROACH 1:- USING BRUTE FORCE APPROACH
         // T.C :- O(n^2) , S.C :- O(1)
-        return a1(word,numFriends);
+        // return a1(word,numFriends);
 
         // APPROACH 2:- MOST OPTIMISED APPROACH
         // T.C :- O(n) , S.C :- O(n)
-        // return a2(word,numFriends);
+        return a2(word,numFriends);
         
     }
 };
