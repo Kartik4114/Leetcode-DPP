7992dcaef358434b77e928c27b79e593b4222f8c
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-06-07 18:00:36 +0530

    Time: 127 ms (60.39%), Space: 26 MB (78.9%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,34 +1,73 @@
 class Solution {
 public:
-    string clearStars(string s) {
-        int n = s.size();
-        map<char, set<int>> mp;
 
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '*') {
-                // Find the smallest character in the map (lexicographically)
-                auto& st = mp.begin()->second; // Use reference so original set is modified
-                auto it = prev(st.end());      // Get last inserted index
-                s[*it]='*';
-                
-                st.erase(it);    
-                if (st.empty()) {
-                    mp.erase(mp.begin());      // Remove char entry if no indices left
+    // void a1(string &s){
+
+    //     int n = s.size();
+    //     map<char, set<int>> mp;
+
+    //     for (int i = 0; i < n; i++) {
+    //         if (s[i] == '*') {
+    //             // Find the smallest character in the map (lexicographically)
+    //             auto& st = mp.begin()->second; // Use reference so original set is modified
+    //             auto it = prev(st.end());      // Get last inserted index
+    //             s[*it]='*';
+
+    //             st.erase(it);    
+    //             if (st.empty()) {
+    //                 mp.erase(mp.begin());      // Remove char entry if no indices left
+    //             }
+    //         } else {
+    //             mp[s[i]].insert(i); // Track index for each character
+    //         }
+    //     }
+
+    // }
+    
+    typedef pair<char,int> P; 
+    
+    class comp{
+        
+        public: 
+            bool operator()(P &p1,P &p2){
+                if(p1.first==p2.first){
+                    return p1.second<p2.second;
                 }
+
+                return p1.first>p2.first;
+            }
+    };
+
+    void a2(string &s){
+
+        int n=s.size();
+
+        priority_queue<P,vector<P>,comp> pq;
+
+        for(int i=0;i<n;i++){
+
+            if(s[i]=='*'){
+
+                auto top=pq.top();
+                pq.pop();
+
+                int idx=top.second;
+                s[idx]='*';
             } else {
-                mp[s[i]].insert(i); // Track index for each character
+                pq.push({s[i],i});
             }
         }
 
-        // vector<pair<int,int>> temp;
+    }
+    string clearStars(string s) {
+        
+        // APPROACH 1:- USING MAP (CHAR+SET) 
+        // T.C :- O(NLOGN) , S.C :- O(N)
+        // a1(s);
 
-        // for(auto &[ch,indices]:mp){
-        //     for(auto &idx:indices){
-        //         temp.push_back({idx,ch});
-        //     }
-        // }
-
-        // sort(temp.begin(),temp.end());
+        // APPROACH 2:- USING priority queue 
+        // T.C :- O(NLOGN) , S.C :- O(N)
+        a2(s);
 
         string result="";
         for(auto &ch:s){
@@ -37,7 +76,6 @@ public:
                 result+=ch;
             }
         }
-
         return result;
     }
 };
