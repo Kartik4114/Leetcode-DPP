7531cd947108e3482803e1b08c03004411a8b956
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-06-08 11:43:58 +0530

    Time: 392 ms (5.44%), Space: 16 MB (21.86%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,55 +1,20 @@
 class Solution {
 public:
-    void solve(int n,int lastNum,int idx,vector<int> &result){
-        if(idx>=n) return;
-        if(lastNum>n) return;
-        
-        result.push_back(lastNum);
-        for(int i=0;i<=9;i++){
+    const static bool comp(int x,int y){
 
-            int nextNum=lastNum*10+i;
-            if(nextNum<=n){
-                solve(n,nextNum,i,result);
-            }
-        }
-    }
+        string str_x=to_string(x);
+        string str_y=to_string(y);
 
-    vector<int> a2(int n){
-
-        vector<int> result;
-        for(int i=1;i<=9;i++){
-            solve(n,i,0,result);
-        }
-
-        return result;
-    }
-
-    vector<int> a1(int n){
-
-        vector<int> result(n);
-        for(int i=0;i<n;i++){
-            result[i]=i+1;
-        }
-
-        auto lambda=[](int x,int y){
-
-            string s1=to_string(x);
-            string s2=to_string(y);
-
-            return s1 < s2;
-        };
-        sort(result.begin(),result.end(),lambda);
-        return result;
+        return str_x<str_y;
     }
     vector<int> lexicalOrder(int n) {
         
-        // APPROACH 1:- USING SORTING 
-        // T.C :- O(nlogn)
-        return a1(n);
+        vector<int> result(n);
+        for(int i=1;i<=n;i++){
+            result[i-1]=i;
+        }
 
-        // APPROACH 2:- USING RECURSION 
-        // T.C :- O(N) AS we are visiting every element only once
-        // return a2(n);
-
+        sort(result.begin(),result.end(),comp);
+        return result;
     }
 };
