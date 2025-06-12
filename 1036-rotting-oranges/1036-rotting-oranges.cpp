4de989885c28969c6468872c6f219ae09f3769f2
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-06-12 14:29:20 +0530

    Time: 0 ms (100%), Space: 16.9 MB (55.33%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -8,31 +8,30 @@ public:
 
         queue<pair<int,int>> que;
 
-        int freshOgs=0;
-
+        int freshOrgs=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-
                 if(grid[i][j]==2){
                     que.push({i,j});
                 } else if(grid[i][j]==1){
-                    freshOgs++;
+                    freshOrgs++;
                 }
             }
         }
 
-        if(freshOgs==0) return 0;
+        if(freshOrgs==0) return 0;
+
         int minutes=0;
         while(!que.empty()){
 
             int N=que.size();
-
             while(N--){
-
-                int x=que.front().first;
-                int y=que.front().second;
+                
+                auto front=que.front();
                 que.pop();
 
+                int x=front.first;
+                int y=front.second;                    
                 for(auto &dir:directions){
 
                     int new_x=x+dir[0];
@@ -41,13 +40,14 @@ public:
                     if(new_x>=0 && new_x<m && new_y>=0 && new_y<n && grid[new_x][new_y]==1){
                         grid[new_x][new_y]=2;
                         que.push({new_x,new_y});
-                        freshOgs--;
+                        freshOrgs--;
                     }
                 }
             }
+
             minutes++;
         }
 
-        return (freshOgs==0) ? (minutes-1) : -1;
+        return (freshOrgs==0) ? minutes-1 : -1;
     }
 };
