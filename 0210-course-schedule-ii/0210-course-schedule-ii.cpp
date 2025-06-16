4dd8aa5998844d52be38c01f2ebc3cbe22e6f0ff
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-06-17 01:02:45 +0530

    Time: 3 ms (73.63%), Space: 19 MB (21.89%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,113 +1,64 @@
 class Solution {
 public:
-    vector<int> topologicalSort(int n,unordered_map<int,vector<int>> &adj,vector<int> &inDegree){
-        queue<int> que;
+    bool hasCycle=false;
 
-        vector<int> result;
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(inDegree[i]==0){
-                result.push_back(i);
-                que.push(i);
-                cnt++;
-            }
-        }
+    void check(unordered_map<int,vector<int>> &adj,vector<bool> &visited,vector<bool> &inRecursion,int node,stack<int>&st){
 
-        while(!que.empty()){
-            int u=que.front();
-            que.pop();
+        visited[node]=true;
+        inRecursion[node]=true;
 
-            for(auto &v:adj[u]){
+        for(auto &v:adj[node]){
 
-                inDegree[v]--;
-                if(inDegree[v]==0){
-                    result.push_back(v);
-                    que.push(v);
-                    cnt++;
-                }
-            }
-        }
-
-        if(cnt==n) return result;
-        return {};
-    }
-    vector<int> a1(int numCourses, vector<vector<int>>& prerequisites){
-        unordered_map<int,vector<int>> adj;
-
-        vector<int> inDegree(numCourses,0);
-
-        for(auto &vec:prerequisites){
-            int a=vec[0];
-            int b=vec[1];
-
-            adj[b].push_back(a);
-            inDegree[a]++;
-        }
-
-        return topologicalSort(numCourses,adj,inDegree);
-    }
-    
-    bool hasCycle;
-    void isCycleDFS(unordered_map<int,vector<int>> &adj,vector<bool> &visited,int u,
-                    stack<int> &st,vector<bool> &inRecursion){
-        visited[u]=true;
-        inRecursion[u]=true;
-
-        for(auto &v:adj[u]){
-            if(!visited[v]){   
-                isCycleDFS(adj,visited,v,st,inRecursion);
+            if(!visited[v]){
+                check(adj,visited,inRecursion,v,st);
             }
 
-            else if(inRecursion[v]==true){
+            if(inRecursion[v]==true){
                 hasCycle=true;
                 return;
             }
         }
-        st.push(u);
-        inRecursion[u]=false;
+        st.push(node);
+        inRecursion[node]=false;
         return;
-
     }
-    vector<int> a2(int numCourses, vector<vector<int>>& prerequisites){
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        
         unordered_map<int,vector<int>> adj;
 
-        vector<bool> visited(numCourses,false);
-        vector<bool> inRecursion(numCourses,false);
+        for(auto &edge:prerequisites){
 
-        for(auto &vec:prerequisites){
-            int a=vec[0];
-            int b=vec[1];
+            int u=edge[0];
+            int v=edge[1];
 
-            adj[b].push_back(a);
+            adj[v].push_back(u);
         }
 
         stack<int> st;
+        vector<bool> visited(numCourses,false);
+        vector<bool> inRecursion(numCourses,false);
+
         for(int i=0;i<numCourses;i++){
 
-            if(!visited[i])
-            isCycleDFS(adj,visited,i,st,inRecursion);
+            if(!visited[i]){
+                check(adj,visited,inRecursion,i,st);
+            }
         }
+
+        if(hasCycle) return {};
 
         vector<int> result;
         while(!st.empty()){
+
             result.push_back(st.top());
             st.pop();
         }
-        if(hasCycle==true) return {};
+
         return result;
-        
-    }
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
 
-        // APPROACH 1:- USING BFS
-        // IT IS DETECT CYCLE IN DIRECTED GRAPH(KAHN'S ALGORITHM)
 
-        // return a1(numCourses,prerequisites);
 
-        // APPROACH 2:- USING DFS
 
-        // IT IS DETECT CYCLE IN DIRECTED GRAPH
-        return a2(numCourses,prerequisites);
 
     }
 };
