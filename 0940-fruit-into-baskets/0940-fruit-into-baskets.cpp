cf8df0402f182b1a19ad02592ed9a4ed2d43bb9a
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-08-04 15:43:09 +0530

    Time: 38 ms (66.11%), Space: 75 MB (77.36%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -9,24 +9,24 @@ public:
         int i=0;
         int j=0;
 
-        int maxFruitCnt=0;
+        int maxResult=0;
         while(j<n){
-
+            
             mp[fruits[j]]++;
 
             while(i<n && mp.size()>2){
-
                 mp[fruits[i]]--;
+
                 if(mp[fruits[i]]==0){
                     mp.erase(fruits[i]);
                 }
                 i++;
             }
-            
-            maxFruitCnt=max(maxFruitCnt,j-i+1);
+
+            maxResult=max(maxResult,j-i+1);
             j++;
         }
 
-        return maxFruitCnt;
+        return maxResult;
     }
 };
