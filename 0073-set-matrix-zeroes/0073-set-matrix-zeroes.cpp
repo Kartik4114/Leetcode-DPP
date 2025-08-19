101b253cb1527a864dbc10afeaa81c57b77816d8
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-08-19 15:26:29 +0530

    Time: 0 ms (100%), Space: 18.7 MB (51.41%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,29 +5,44 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
 
-        unordered_set<int> row;
-        unordered_set<int> col;
-
+        int col0=1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
 
                 if(matrix[i][j]==0){
-                    row.insert(i);
-                    col.insert(j);
+
+                    matrix[i][0]=0;
+
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    } else {
+                        col0=0;
+                    }
                 }
             }
         }
 
-        for(int i=0;i<m;i++){
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+
+                if(matrix[i][j]!=0){
+
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+
+        if(matrix[0][0]==0){
             for(int j=0;j<n;j++){
+                matrix[0][j]=0;
+            }
+        }
 
-                if((row.find(i)!=row.end())) {
-                    matrix[i][j]=0;
-                }
-
-                if((col.find(j)!=col.end())) {
-                    matrix[i][j]=0;
-                }
+        if (col0 == 0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
