da4fea63c71d811e435bb4a516543760d873dfda
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-09-14 11:52:56 +0530

    Time: 2 ms (5.58%), Space: 10.9 MB (92.35%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,85 +11,36 @@
  */
 class Solution {
 public:
-    vector<int> ans;
-    void a1(TreeNode* root){
-        if(root==NULL) return;
-
-        a1(root->left);
-        a1(root->right);
-        ans.push_back(root->val);
-    }
-
-    void a2(TreeNode* root){
-        
-        if(root==NULL) return;
-        TreeNode* node=root;
-        stack<TreeNode*> st;
-        stack<int> resultSt;
-
-        st.push(node);
-        while(!st.empty()){
-            auto top=st.top();
-            st.pop();
-
-            resultSt.push(top->val);
-            if(top->left!=NULL){
-                st.push(top->left);
-            }
-            if(top->right!=NULL){
-                st.push(top->right);
-            }
-        }
-
-        while(!resultSt.empty()){
-            ans.push_back(resultSt.top());
-            resultSt.pop();
-        }
-    }
-
-    void a3(TreeNode* root){
-        
-        TreeNode*curr=root;
-        stack<TreeNode*> st;
-
-        while(curr!=NULL || !st.empty()) {
-
-            if(curr!=NULL){
-                st.push(curr);
-                curr=curr->left;
-            } else {
-
-                TreeNode* temp=st.top()->right;
-
-                if(temp==NULL){
-                    temp=st.top();
-                    st.pop();
-
-                    ans.push_back(temp->val);
-
-                    while(!st.empty() && temp==st.top()->right){
-                        temp=st.top();
-                        st.pop();
-                        ans.push_back(temp->val);
-                    }
-                } else {
-                    curr=temp;
-                }
-
-            }
-        }
-    }
     vector<int> postorderTraversal(TreeNode* root) {
+        
+        if(root==NULL) return {};
 
-    // APPROACH 1:- USING RECURSION 
-    //    a1(root);
-    
-    // APPROACH 2:- USING ITERATIVE (2 STACKS)
-        // a2(root);
-    //    return ans;
-    
-    // APPROACH 3:- USING ITERTATIVE (1 STACK)
-        a3(root);
-        return ans;
+        TreeNode*node=root;
+        stack<TreeNode*> st1;
+        stack<TreeNode*> st2;
+
+        st1.push(node);
+
+        while(!st1.empty()){
+
+            auto top=st1.top();
+            st1.pop();
+
+            st2.push(top);
+
+            if(top->left!=NULL) st1.push(top->left);
+            if(top->right!=NULL) st1.push(top->right);
+
+        }
+
+        vector<int> result;
+
+        while(!st2.empty()){
+
+            result.push_back(st2.top()->val);
+            st2.pop();
+        }
+
+        return result;
     }
 };
