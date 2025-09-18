c5141c5e723642cc6a19fbdcad0fb04a6d5875eb
Kartik Mittal <148052939+Kartik4114@users.noreply.github.com>
2025-09-19 01:23:18 +0530

    Time: 0 ms (100%), Space: 97.4 MB (60.5%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,44 +1,20 @@
 class Solution {
 public:
-    int a1(vector<int>& prices){
-
-        int n=prices.size();
-        int result=0;
-
-        int curr=prices[0];
-        for(int i=1;i<n;i++){
-            
-            if(prices[i]>curr){
-                result=max(result,prices[i]-curr);
-            } else {
-                curr=prices[i];
-            }
-        }
-        return result;
-    }
-
-    int a2(vector<int>& prices){
-
-        int n=prices.size();
-        int maxProfit=0;
-        int mini=prices[0];
-
-        for(int i=1;i<n;i++){
-
-            int cost=prices[i]-mini;
-            maxProfit=max(maxProfit,cost);
-            mini=min(mini,prices[i]);
-
-        }
-        return maxProfit;
-    }
     int maxProfit(vector<int>& prices) {
         
-        // APPROACH 1:- MY APPROACH
-        // return a1(prices);
+        int n=prices.size();
 
-        // APPROACH 2:-
-        return a2(prices);
-        
+        int minEl=prices[0];
+
+        int maxi=0;
+
+        for(int i=1;i<n;i++){
+
+            int curr=prices[i]-minEl;
+            maxi=max(maxi,curr);
+            minEl=min(minEl,prices[i]);
+        }
+
+        return maxi;
     }
 };
